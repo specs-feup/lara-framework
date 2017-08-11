9ef6c7ad897854dd23cc7826cc9df8e2cd3e80d2
JoaoBispo <joaobispo@gmail.com>
2017-08-11 01:41:29 +0100

    [LaraApi] Adds project

---
+++ LaraApi/src-java/pt/up/fe/specs/lara/LaraApiResource.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2013 SuikaSoft.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.lara;
+
+import org.lara.interpreter.weaver.utils.LaraResourceProvider;
+
+/**
+ * @author Joao Bispo
+ *
+ */
+public enum LaraApiResource implements LaraResourceProvider {
+    // Code
+    ENERGY_BASE("code/EnergyBase.lara"),
+    LOGGER_BASE("code/LoggerBase.lara"),
+    TIMER_BASE("code/TimerBase.lara"),
+
+    // Lara
+    DEBUG("Debug.lara"),
+    IO("Io.lara"),
+    JOIN_POINTS("JoinPoints.lara"),
+    PLATFORMS("Platforms.lara"),
+    SYSTEM("System.lara"),
+
+    // Util
+    ACCUMULATOR("util/Accumulator.lara"),
+    ID_GENERATOR("util/IdGenerator.lara"),
+    PRINT_ONCE("util/PrintOnce.lara"),
+    STRING_SET("util/StringSet.lara"),
+    TIME_UNITS("util/TimeUnits.lara");
+
+    private final String resource;
+
+    private static final String BASE_PACKAGE = "lara/";
+
+    /**
+     * @param resource
+     */
+    private LaraApiResource(String resource) {
+        this.resource = BASE_PACKAGE + resource;
+    }
+
+    /* (non-Javadoc)
+     * @see org.suikasoft.SharedLibrary.Interfaces.ResourceProvider#getResource()
+     */
+    @Override
+    public String getOriginalResource() {
+        return resource;
+    }
+
+}
---
+++ LaraApi/src-js/lara/LaraCore.js
@@ -0,0 +1,57 @@
+function notImplemented(functionName) {
+	
+	functionName = defaultValue(functionName, "<unknown>");
+	
+	//println("Objects: " + Debug.getObjects(this).join());
+	//println("Weaver functions: " + getFunctions(Weaver).join());
+	
+	throw "Function " + functionName + " not implemented yet for this weaver implementation";
+}
+
+function defaultValue(variable, defaultValue) {
+	return variable === undefined ? defaultValue : variable;
+}
+
+/**
+ * Temporary method that returns true if the given value is undefined or null, 
+ * to be used while WeaverGenerator does not support returning undefined from Java. 
+ */
+function isUndefined(value) {
+	return value === undefined || value === null;
+}
+
+function checkTrue(booleanExpr, message) {
+	if(!booleanExpr) {
+		throw message;
+	}
+}
+
+function checkDefined(value, varName, source) {
+	if(!isUndefined(value)) {
+		return;
+	}
+	
+	// Undefined, throw exception
+	var message = "Value ";
+	if(varName !== undefined) {
+		message += varName + " ";
+	}
+	if(source !== undefined) {
+		message += "from " + source + " ";
+	}
+	message += "is undefined";
+	
+	throw message;
+}
+
+function checkType(value, type) {
+	if(typeof value !== type) {
+		throw "Expected value to be of type '" + type + "', but is of type " + (typeof value);
+	}
+}	 
+
+function toArray(objectWithLength) {
+	return Array.prototype.slice.call(objectWithLength);
+}
+
+
