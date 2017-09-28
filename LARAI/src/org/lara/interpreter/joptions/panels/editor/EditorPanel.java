0a6a65321e4fd4463529fa42d34ee7d0a968e263
Tiago Carvalho <tiagodrcarvalho@gmail.com>
2017-09-28 16:18:00 +0200

    [LARAI] Adds editor persistence and option to not ask again to save

--- LARAI/src/org/lara/interpreter/joptions/panels/editor/EditorPanel.java
+++ LARAI/src/org/lara/interpreter/joptions/panels/editor/EditorPanel.java
@@ -35,13 +35,14 @@ import org.lara.interpreter.joptions.panels.editor.components.SearchPanel;
 import org.lara.interpreter.joptions.panels.editor.listeners.FocusGainedListener;
 import org.lara.interpreter.joptions.panels.editor.tabbed.MainLaraTab;
 import org.lara.interpreter.joptions.panels.editor.tabbed.TabsContainerPanel;
-import org.lara.interpreter.joptions.panels.editor.utils.EditorKeys;
 import org.lara.interpreter.joptions.panels.editor.utils.LaraWorker;
+import org.lara.interpreter.joptions.panels.editor.utils.SettingsManager;
 import org.lara.language.specification.LanguageSpecification;
 import org.suikasoft.jOptions.Interfaces.DataStore;
 import org.suikasoft.jOptions.app.AppPersistence;
 import org.suikasoft.jOptions.gui.panels.app.AppKeys;
 import org.suikasoft.jOptions.gui.panels.app.GuiTab;
+import org.suikasoft.jOptions.gui.panels.app.TabbedPane;
 
 import pt.up.fe.specs.util.SpecsIo;
 import pt.up.fe.specs.util.SpecsLogs;
@@ -60,8 +61,9 @@ public class EditorPanel extends GuiTab {
      */
     private static final long serialVersionUID = 1L;
 
+    private final SettingsManager settings;
     private final TabsContainerPanel tabsContainer;
-    private final LanguageSpecificationSideBar langSpecSideBar;
+    // private final LanguageSpecificationSideBar langSpecSideBar;
     private final AppPersistence persistence;
     private File canonicalAspectFile;
     private DataStore optionsDataStore;
@@ -70,8 +72,8 @@ public class EditorPanel extends GuiTab {
     private final JTextArea outputArea;
     private final JScrollPane consolePanel;
     private final JSplitPane splitterConsole;
-    private boolean init = true;
-    private final JComponent lsScrollBar;
+    // private boolean init = true;
+    private final LanguageSpecificationSideBar langSpecSideBar;
     private double lasSplitSize = 0.75;
     private Explorer explorer;
     private final LaraWorker worker;
@@ -79,6 +81,8 @@ public class EditorPanel extends GuiTab {
     // private boolean requiresUpdate = false;
 
     private boolean runDebug = false;
+
+    private JSplitPane splitterExplorer;
     public static final int DEFAULT_FONT = 12;
 
     // public static EditorPanel newInstance(DataStore dataStore) {
@@ -97,7 +101,7 @@ public class EditorPanel extends GuiTab {
     public EditorPanel(DataStore dataStore, AppPersistence persistence, LanguageSpecification langSpec) {
         super(dataStore);
         setLayout(new BorderLayout());
-
+        this.settings = new SettingsManager(this, getAppName());
         this.persistence = persistence;
         canonicalAspectFile = null;
         explorer = new Explorer(this);
@@ -128,38 +132,35 @@ public class EditorPanel extends GuiTab {
         centerPanel.add(tabsContainer, BorderLayout.CENTER);
 
         // lsScrollBar = new JScrollPane(langSpecSideBar);
-        lsScrollBar = langSpecSideBar;
-        // lsScrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS); //avoids unresized
-        // components!
-        // lsScrollBar.setHorizontalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        // JScrollBar verticalScrollBar = lsScrollBar.getVerticalScrollBar();
-        // verticalScrollBar.addAdjustmentListener(l -> {
-        // if (verticalScrollBar.isShowing()) {
-        // if (requiresUpdate) {
-        //
-        // requiresUpdate = false;
-        // langSpecSideBar.resizeForScrollBar();
-        // System.out.println("RESIZE");
-        // }
-        // } else {
-        // if (!requiresUpdate) {
-        // requiresUpdate = true;
-        // System.out.println("RESET FOR RESIZE");
-        // }
-        // }
-        // });
+        // lsScrollBar = langSpecSideBar;
 
-        centerPanel.add(lsScrollBar, BorderLayout.EAST);
+        centerPanel.add(langSpecSideBar, BorderLayout.EAST);
 
         splitterConsole = new JSplitPane(JSplitPane.VERTICAL_SPLIT, centerPanel, consolePanel);
+        splitterConsole.addPropertyChangeListener("dividerLocation", p -> {
+            // System.out.println("changed: " + p.getNewValue());
+            // Only save split factor if the console panel is visible (hiding the console panel makes split factor to be
+            // almost 1.0
+            if (consolePanel.isVisible()) {
+                // System.out.println("Changing size when console is not visible");
+                settings.saveConsoleSplitFactor(getDividerProportion(splitterConsole));
+            }
+        });
         // splitterConsole.setDividerLocation(this.lasSplitSize);
         // splitterConsole.add(centerPanel);
         // splitterConsole.add(consolePanel);
         // add(splitterConsole, BorderLayout.CENTER);
 
-        JSplitPane splitterExplorer = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        splitterExplorer = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         splitterExplorer.add(explorer);
         splitterExplorer.add(splitterConsole);
+        splitterExplorer.addPropertyChangeListener("dividerLocation", p -> {
+            // System.out.println("changed: " + p.getNewValue());
+            double dividerProportion = getDividerProportion(splitterExplorer);
+            settings.saveExplorerSplitFactor(dividerProportion);
+            // System.out.println("Exp. SAVE: " + dividerProportion);
+        });
+
         add(splitterExplorer, BorderLayout.CENTER);
 
         add(searchPanel, BorderLayout.SOUTH);
@@ -179,17 +180,14 @@ public class EditorPanel extends GuiTab {
 
     @Override
     public void enterTab() {
-        if (init) {
-            splitterConsole.setDividerLocation(lasSplitSize);
-        }
+
+        loadEditorPreferences();
+
         optionsDataStore = null;
         outputFile = null;
 
         if (!getData().hasValue(AppKeys.CONFIG_FILE)) {
             menu.deativateExecButton();
-            // if (init) {
-            // splitterConsole.setDividerLocation(lasSplitSize);
-            // }
             return;
         }
         outputFile = getData().get(AppKeys.CONFIG_FILE);
@@ -198,13 +196,9 @@ public class EditorPanel extends GuiTab {
 
         if (optionsDataStore == null) {
             menu.deativateExecButton();
-            // if (init) {
-            // splitterConsole.setDividerLocation(lasSplitSize);
-            // }
             return;
         }
         menu.ativateExecButton();
-        loadEditorProperties();
 
         // DataStore setup = application.getPersistence().loadData(file);
         updateProjects(optionsDataStore);
@@ -212,28 +206,43 @@ public class EditorPanel extends GuiTab {
         explorer.refreshAllExceptMain();
     }
 
-    private void loadEditorProperties() {
-        if (optionsDataStore.hasValue(EditorKeys.splitSize)) {
-            Double proportionalLocation = optionsDataStore.get(EditorKeys.splitSize);
-            System.out.println("LAST: " + proportionalLocation);
-            splitterConsole.setDividerLocation(proportionalLocation);
-            // System.out.println("SPLIT: " + proportionalLocation);
-        } else {
-            if (init) {
-                splitterConsole.setDividerLocation(lasSplitSize);
-                init = false;
-            }
-        }
+    private void loadEditorPreferences() {
 
-        if (optionsDataStore.hasValue(EditorKeys.isBarShown)) {
-            lsScrollBar.setVisible(optionsDataStore.get(EditorKeys.isBarShown));
-            revalidate();
-        }
-        if (optionsDataStore.hasValue(EditorKeys.isOutputShown)) {
-            lsScrollBar.setVisible(optionsDataStore.get(EditorKeys.isOutputShown));
-            revalidate();
-        }
+        lasSplitSize = settings.loadConsoleSplitFactor(0.75);
+        splitterConsole.setDividerLocation(lasSplitSize);
+        double newSplitFactor = settings.loadExplorerSplitFactor(0.25);
+        // System.out.println("Exp. LOAD: " + newSplitFactor);
+        splitterExplorer.setDividerLocation(newSplitFactor);
+        boolean showConsole = settings.loadShowConsole(true);
+        consolePanel.setVisible(showConsole);
+        boolean showLangSpec = settings.loadShowLangSpec(true);
+        langSpecSideBar.setVisible(showLangSpec);
+        revalidate();
     }
+
+    //
+    // private void loadEditorProperties() {
+    // if (optionsDataStore.hasValue(EditorKeys.splitSize)) {
+    // Double proportionalLocation = optionsDataStore.get(EditorKeys.splitSize);
+    // System.out.println("LAST: " + proportionalLocation);
+    // splitterConsole.setDividerLocation(proportionalLocation);
+    // // System.out.println("SPLIT: " + proportionalLocation);
+    // } else {
+    // if (init) {
+    // splitterConsole.setDividerLocation(lasSplitSize);
+    // init = false;
+    // }
+    // }
+    //
+    // if (optionsDataStore.hasValue(EditorKeys.isBarShown)) {
+    // lsScrollBar.setVisible(optionsDataStore.get(EditorKeys.isBarShown));
+    // revalidate();
+    // }
+    // if (optionsDataStore.hasValue(EditorKeys.isOutputShown)) {
+    // lsScrollBar.setVisible(optionsDataStore.get(EditorKeys.isOutputShown));
+    // revalidate();
+    // }
+    // }
 
     private DataStore extractDataStore(File file) {
 
@@ -286,7 +295,7 @@ public class EditorPanel extends GuiTab {
 
     @Override
     public void exitTab() {
-        lasSplitSize = getDividerProportion();
+
         if (outputFile == null || optionsDataStore == null) {
             MainLaraTab mainTab = tabsContainer.getMainTab();
             if (!mainTab.isNew()) {
@@ -296,7 +305,18 @@ public class EditorPanel extends GuiTab {
             return;
         }
 
+        // saveEditorPreferences();
         updateDataStore();
+    }
+
+    // private void saveEditorPreferences() {
+    // String appName = getAppName();
+    // double lasSplitSize = getDividerProportion();
+    // Preferences.userRoot().putDouble(CONSOLE_SPLIT_FACTOR_PREFIX + appName, lasSplitSize);
+    // }
+
+    private String getAppName() {
+        return getData().get(TabbedPane.getAppNameKey());
     }
 
     public void updateDataStore() {
@@ -326,11 +346,26 @@ public class EditorPanel extends GuiTab {
     // optionsDataStore.setRaw(EditorKeys.isOutputShown, outputArea.isVisible());
     // }
 
-    public double getDividerProportion() {
-        int location = splitterConsole.getDividerLocation();
-        int height = splitterConsole.getHeight();
-        int divSize = splitterConsole.getDividerSize();
+    // public double getConsoleDividerProportion() {
+    // return getDividerProportion(splitterConsole);
+    // }
+    //
+    // public double getConsoleDividerProportion() {
+    // return getDividerProportion(splitterConsole);
+    // }
+
+    public double getDividerProportion(JSplitPane splitter) {
+        int orientation = splitter.getOrientation();
+        int location = splitter.getDividerLocation();
+        if (orientation == JSplitPane.HORIZONTAL_SPLIT) {
+            int width = splitter.getWidth();
+            int divSize = splitter.getDividerSize();
+            return location / (double) (width - divSize);
+        }
+        int height = splitter.getHeight();
+        int divSize = splitter.getDividerSize();
         return location / (double) (height - divSize);
+
     }
 
     @Override
@@ -355,29 +390,32 @@ public class EditorPanel extends GuiTab {
     // }
 
     public JComponent getLsScrollBar() {
-        return lsScrollBar;
+        return langSpecSideBar;
     }
 
-    public double getLasSplitSize() {
-        return lasSplitSize;
-    }
+    // public double getLasSplitSize() {
+    // return lasSplitSize;
+    // }
 
-    public void setLasSplitSize(double lasSplitSize) {
-        this.lasSplitSize = lasSplitSize;
-    }
+    // public void setLasSplitSize(double lasSplitSize) {
+    // this.lasSplitSize = lasSplitSize;
+    // }
 
-    public JSplitPane getSplitter() {
-        return splitterConsole;
-    }
+    // public JSplitPane getSplitter() {
+    // return splitterConsole;
+    // }
 
     public void swapConsoleVisibility() {
+        // consolePanel.setVisible(!consolePanel.isVisible());
         if (consolePanel.isVisible()) {
-            lasSplitSize = getDividerProportion();
+            lasSplitSize = getDividerProportion(splitterConsole);
             consolePanel.setVisible(false);
         } else {
             consolePanel.setVisible(true);
             splitterConsole.setDividerLocation(lasSplitSize);
         }
+        settings.saveShowConsole(consolePanel.isVisible());
+        // Preferences.userRoot().putBoolean(getShowConsoleSetting(), consolePanel.isVisible());
         revalidate();
     }
 
@@ -429,7 +467,9 @@ public class EditorPanel extends GuiTab {
         // VerboseLevel originalVerbose = optionsDataStore.get(LaraiKeys.VERBOSE);
         // System.out.println(optionsDataStore);
 
-        DataStore tempDS = DataStore.newInstance(optionsDataStore.getName(), optionsDataStore);
+        DataStore tempDS = DataStore.newInstance(
+                optionsDataStore.getStoreDefinition().map(sd -> sd.getName()).orElse(optionsDataStore.getName()),
+                optionsDataStore);
         tempDS.setRaw(LaraiKeys.DEBUG_MODE, true);
         tempDS.setRaw(LaraiKeys.VERBOSE, VerboseLevel.all);
         runDebug = true;
@@ -456,4 +496,9 @@ public class EditorPanel extends GuiTab {
             menu.setStopRun();
         }
     }
+
+    public SettingsManager getSettings() {
+        return settings;
+    }
+
 }
