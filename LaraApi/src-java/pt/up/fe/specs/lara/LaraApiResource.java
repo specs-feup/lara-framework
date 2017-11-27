366f47b9cd6781c521331ca6efc20fc62719465c
pedro <p.pinto@fe.up.pt>
2017-11-27 18:53:59 +0000

    [LaraApi] Adds classes representing modifiers to units

--- LaraApi/src-java/pt/up/fe/specs/lara/LaraApiResource.java
+++ LaraApi/src-java/pt/up/fe/specs/lara/LaraApiResource.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2013 SuikaSoft.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -36,6 +36,15 @@ public enum LaraApiResource implements LaraResourceProvider {
     FILE_SIZE_METRIC("metrics/FileSizeMetric.lara"),
     METRIC("metrics/Metric.lara"),
     METRIC_RESULT("metrics/MetricResult.lara"),
+
+    // Units
+    SI_MODIFIER("units/SiModifier.lara"),
+    SI_UNIT("units/SiUnit.lara"),
+    UNIT("units/Unit.lara"),
+    TIME_MODIFIER("units/TimeModifier.lara"),
+    TIME_UNIT("units/TimeUnit.lara"),
+    UNIT_MODIFIER("units/UnitModifier.lara"),
+    UNIT_WITH_MODIFIER("units/UnitWithModifier.lara"),
 
     // Lara
     CSV("Csv.lara"),
