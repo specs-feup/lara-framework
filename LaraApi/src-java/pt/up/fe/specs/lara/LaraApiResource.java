8e2a16c17e75b85d5ad0b67b0d3de0dcd8da987f
JoaoBispo <joaobispo@gmail.com>
2018-07-17 03:24:24 +0100

    [LaraApi] Adds lara.util.Combinations

--- LaraApi/src-java/pt/up/fe/specs/lara/LaraApiResource.java
+++ LaraApi/src-java/pt/up/fe/specs/lara/LaraApiResource.java
@@ -78,6 +78,7 @@ public enum LaraApiResource implements LaraResourceProvider {
     // Util
     ACCUMULATOR("util/Accumulator.lara"),
     CHECKPOINT("util/Checkpoint.lara"),
+    COMBINATION("util/Combinations.lara"),
     DATA_STORE("util/DataStore.lara"),
     ID_GENERATOR("util/IdGenerator.lara"),
     LINE_INSERTER("util/LineInserter.lara"),
