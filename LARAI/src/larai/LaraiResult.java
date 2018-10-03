8967d5ed6a4edd403653a9de6c5fac4d28476e34
JoaoBispo <joaobispo@gmail.com>
2018-10-03 18:52:30 +0100

    [LARAI] Adds static method LaraI.isRunningGui()

---
+++ LARAI/src/larai/LaraiResult.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2018 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package larai;
+
+import org.suikasoft.jOptions.DataStore.ADataClass;
+import org.suikasoft.jOptions.Datakey.DataKey;
+import org.suikasoft.jOptions.Datakey.KeyFactory;
+
+public class LaraiResult extends ADataClass<LaraiResult> {
+
+    public final static DataKey<Boolean> IS_SUCCESS = KeyFactory.bool("isSuccess");
+    public final static DataKey<Boolean> IS_RUNNING_GUI = KeyFactory.bool("isRunningGui");
+
+    public static LaraiResult newInstance(boolean isSuccess, boolean isRunningGui) {
+        return new LaraiResult().set(IS_SUCCESS, isSuccess).set(IS_RUNNING_GUI, isRunningGui);
+    }
+}
