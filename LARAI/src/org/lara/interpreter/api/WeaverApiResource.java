d78cc35906e478c93c584d54e8efded595a46011
JoaoBispo <joaobispo@gmail.com>
2019-04-16 23:06:16 +0100

    [LARAI] Adds weaver.Weaver, moves WeaverOptions.getWeaver there

--- LARAI/src/org/lara/interpreter/api/WeaverApiResource.java
+++ LARAI/src/org/lara/interpreter/api/WeaverApiResource.java
@@ -27,6 +27,7 @@ public enum WeaverApiResource implements LaraResourceProvider {
     // Weaver
     JOIN_POINTS("JoinPointsBase.lara"),
     SELECTOR("Selector.lara"),
+    WEAVER("Weaver.lara"),
     WEAVER_JPS("WeaverJps.lara"),
     WEAVER_LAUNCHER_BASE("WeaverLauncherBase.lara"),
     WEAVER_OPTIONS("WeaverOptions.lara");
