bd9700a983babde5a828da3b1f4cd8593c655e7c
JoaoBispo <joaobispo@gmail.com>
2019-05-27 10:38:31 +0100

    [LARAI] Adapts code to insert action now returning JoinPoint[]

--- LARAI/src/org/lara/interpreter/weaver/defaultweaver/joinpoints/DWFunction.java
+++ LARAI/src/org/lara/interpreter/weaver/defaultweaver/joinpoints/DWFunction.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2013 SPeCS.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -13,6 +13,7 @@
 package org.lara.interpreter.weaver.defaultweaver.joinpoints;
 
 import org.lara.interpreter.weaver.defaultweaver.abstracts.joinpoints.AFunction;
+import org.lara.interpreter.weaver.interf.JoinPoint;
 
 public class DWFunction extends AFunction {
 
@@ -36,11 +37,13 @@ public class DWFunction extends AFunction {
     // }
 
     @Override
-    public void insertImpl(String position, String code) {
+    public JoinPoint[] insertImpl(String position, String code) {
         System.out.println("#########INSERTING#########");
         System.out.println(
                 "Action not available. But would insert " + position + " function " + name + ": " + code.trim());//
         System.out.println("###########################");
+
+        return null;
     }
 
     // @Override
