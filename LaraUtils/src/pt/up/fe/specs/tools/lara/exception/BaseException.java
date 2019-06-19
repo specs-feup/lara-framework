6cc2eb51342539425890c301a7cc712591a747b2
JoaoBispo <joaobispo@gmail.com>
2019-06-19 02:23:59 +0100

    [LaraUtils] Remove Java 8 compliance, tests on BaseException

--- LaraUtils/src/pt/up/fe/specs/tools/lara/exception/BaseException.java
+++ LaraUtils/src/pt/up/fe/specs/tools/lara/exception/BaseException.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2015 SPeCS Research Group.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -17,7 +17,7 @@ import pt.up.fe.specs.tools.lara.trace.CallStackTrace;
 
 /**
  * Abstract exception of a LARA exception. These type of exceptions are used to encapsulate other thrown exceptions
- * 
+ *
  * @author Tiago
  *
  */
@@ -26,15 +26,19 @@ public abstract class BaseException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     /**
-     * 
+     *
      */
     public BaseException(Throwable e) {
         super(e);
     }
 
+    public BaseException(String message, Throwable e) {
+        super(message, e);
+    }
+
     /**
      * Generate a complete message to present to the user, usually containing 'Exception on' + generateSimpleMessage()
-     * 
+     *
      * @return
      */
     protected abstract String generateMessage();
@@ -42,24 +46,48 @@ public abstract class BaseException extends RuntimeException {
     /**
      * Generate the concrete message to present to the user. Usually this method does not contain the 'Exception on'
      * stuff, just get to the point!
-     * 
+     *
      * @return
      */
     protected abstract String generateSimpleMessage();
 
     @Override
     public String getMessage() {
-        return generateMessage();
+        // return generateMessage();
+        String message = generateMessage();
+
+        // Throwable primaryCause = this;
+        // while (primaryCause.getCause() != null) {
+        // primaryCause = primaryCause.getCause();
+        // }
+        //
+        // if (primaryCause != this) {
+        // StringBuilder cause = new StringBuilder();
+        // cause.append("Primary cause:");
+        // for (var element : primaryCause.getStackTrace()) {
+        // cause.append("\n").append(element);
+        // }
+        //
+        // message += "\n" + cause.toString();
+        // }
+
+        // System.out.println("MESSAGE: " + message);
+        // message += "\n Something extra";
+        return message;
     }
 
     public String getSimpleMessage() {
         return generateSimpleMessage();
+        // String message = generateSimpleMessage();
+        // System.out.println("MESSAGE: " + message);
+        // message += "\n Something extra";
+        // return message;
     }
 
     /**
      * Generate a Runtime exception and use a specific stack trace instead of the one generated based on the chained
      * exceptions
-     * 
+     *
      * @param stackStrace
      * @return
      */
