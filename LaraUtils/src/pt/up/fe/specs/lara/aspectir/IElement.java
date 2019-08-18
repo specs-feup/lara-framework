1ab0ee48f6bbe4706927c6bb7b344577c444401d
JoaoBispo <joaobispo@gmail.com>
2019-08-18 10:43:22 +0100

    [LaraUtils] Adds IElement.getAncestor()

--- LaraUtils/src/pt/up/fe/specs/lara/aspectir/IElement.java
+++ LaraUtils/src/pt/up/fe/specs/lara/aspectir/IElement.java
@@ -1,4 +1,5 @@
 package pt.up.fe.specs.lara.aspectir;
+
 import java.io.PrintStream;
 
 import org.w3c.dom.Document;
@@ -6,11 +7,37 @@ import org.w3c.dom.Element;
 
 /****************************** Interface IElement ******************************/
 public interface IElement extends Visitable {
-	public void loadXml(Element e, String rootName, Document doc) throws Exception;
-	public IElement getParent();
-	public void writeXml(Document doc, Element parent, String  rootName, int level);
-	public void print(PrintStream os, int indent);
-	public String typeName();
-	public void printIndent(PrintStream os, int indent);
+    public void loadXml(Element e, String rootName, Document doc) throws Exception;
+
+    public IElement getParent();
+
+    public void writeXml(Document doc, Element parent, String rootName, int level);
+
+    public void print(PrintStream os, int indent);
+
+    public String typeName();
+
+    public void printIndent(PrintStream os, int indent);
+
+    /**
+     * 
+     * @param <T>
+     * @param ancestorClass
+     * @return an ancestor of the given class, or null if none was found
+     */
+    default <T extends IElement> T getAncestor(Class<T> ancestorClass) {
+        IElement currentElement = this;
+        IElement currentParent = null;
+        while ((currentParent = currentElement.getParent()) != null) {
+            if (ancestorClass.isInstance(currentParent)) {
+                return ancestorClass.cast(currentParent);
+            }
+
+            currentElement = currentParent;
+        }
+
+        // No parent found
+        return null;
+    }
 }
 /********************************************************************************/
