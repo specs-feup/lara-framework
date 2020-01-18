0d9df1956b9eaef3ff89f82a5ea55d1dbbcd4d5f
JoaoBispo <joaobispo@gmail.com>
2020-01-18 21:20:51 +0000

    [LaraParser] Adds JavaCC-based LARA parser

---
+++ LaraParser/src/pt/up/fe/specs/lara/parser/JavaCCLaraParser.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2020 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.lara.parser;
+
+import java.io.File;
+import java.io.InputStream;
+
+import com.google.gson.JsonObject;
+
+import pt.up.fe.specs.lara.parser.javacc.ast.ASTStart;
+import pt.up.fe.specs.lara.parser.javacc.ast.LARAEcmaScript;
+import pt.up.fe.specs.lara.parser.javacc.ast.ParseException;
+import pt.up.fe.specs.util.SpecsIo;
+
+public class JavaCCLaraParser {
+
+    public JsonObject parseFile(File sourceFile) {
+        return parseFile(SpecsIo.toInputStream(sourceFile), SpecsIo.getCanonicalPath(sourceFile));
+    }
+
+    public JsonObject parseFile(InputStream code, String codeSource) {
+
+        // Create parser
+        LARAEcmaScript parser = new LARAEcmaScript(code);
+
+        try {
+            // Parse code
+            ASTStart root = parser.parse();
+
+            // Convert JJTree to EcmaAst
+            System.out.println("AST:");
+            root.dump("");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+}
