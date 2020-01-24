d2eacc6d5b5edc21b8f50b36049baac12e814407
JoaoBispo <joaobispo@gmail.com>
2020-01-24 01:43:01 +0000

    Moves LaraNodeClassesService from project LaraAst to LaraParser

--- LaraParser/src/pt/up/fe/specs/lara/parser/esprima/LaraNodeClassesService.java
+++ LaraParser/src/pt/up/fe/specs/lara/parser/esprima/LaraNodeClassesService.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License. under the License.
  */
 
-package pt.up.fe.specs.lara.ast.utils;
+package pt.up.fe.specs.lara.parser.esprima;
 
 import java.util.Arrays;
 import java.util.Collection;
