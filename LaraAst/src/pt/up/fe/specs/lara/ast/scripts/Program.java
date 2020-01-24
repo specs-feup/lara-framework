3b841f0593ef2d6a238b5c05a1ec009ab9ff8ea9
JoaoBispo <joaobispo@gmail.com>
2020-01-24 02:23:18 +0000

    [LaraAst] Adds package 'scripts'

---
+++ LaraAst/src/pt/up/fe/specs/lara/ast/scripts/Program.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2020 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.lara.ast.scripts;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.suikasoft.jOptions.Interfaces.DataStore;
+
+import pt.up.fe.specs.lara.ast.LaraNode;
+
+/**
+ * A program can be either a script or a module.
+ * 
+ * @author JoaoBispo
+ *
+ */
+public abstract class Program extends LaraNode {
+
+    public Program(DataStore data, Collection<? extends LaraNode> children) {
+        super(data, children);
+    }
+
+    public List<LaraNode> getBody() {
+        return getChildren();
+    }
+
+}
