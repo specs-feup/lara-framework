70ba44c41dc6637ca2349710c7d92a35bf84a1bf
JoaoBispo <joaobispo@gmail.com>
2020-01-24 20:56:25 +0000

    [LaraParser] Adds JavaScript parser tests

---
+++ LaraParser/resources/pt/up/fe/specs/lara/parser/test/js/script.js
@@ -0,0 +1,12 @@
+// Literals
+nullLiteral = null;
+regexLiteral = /a*b/;
+stringLiteral = "Hello World";
+numberLiteral = 20;
+booleanLiteral = true;
+
+
+
+// const a = [[1,2],3];
+// const [[x, y], z] = a;
+// console.log(x, y, z);
