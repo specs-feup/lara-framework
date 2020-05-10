ead8d25c89f75e82f795bf207c40545c0dc17a9c
João Bispo <joaobispo@gmail.com>
2020-05-10 22:57:56 +0100

    [LanguageSpecification] Adds resources folder, starts test for LangSpec2

---
+++ LanguageSpecification/test/pt/up/fe/specs/lara/languagespecification/LanguageSpecificationTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2020 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.lara.languagespecification;
+
+import java.io.File;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.util.SpecsSystem;
+import pt.up.fe.specs.util.providers.ResourceProvider;
+import pt.up.fe.specs.util.xml.XmlDocument;
+
+public class LanguageSpecificationTest {
+
+    @BeforeClass
+    public static void init() {
+        SpecsSystem.programStandardInit();
+    }
+
+    @Test
+    public void testSchema() {
+        var xmlDoc = SpecsIo.toInputStream(new File("language/joinPointModel.xml"));
+        ResourceProvider schema = () -> "schemas/joinPointModel.xsd";
+
+        var root = XmlDocument.newInstance(xmlDoc, SpecsIo.resourceToStream(schema));
+
+        System.out.println("DOC: " + root);
+        // fail("Not yet implemented");
+    }
+
+}
---
+++ LanguageSpecification/test/pt/up/fe/specs/lara/languagespecification/LanguageSpecificationTestResource.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2013 SuikaSoft.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.lara.languagespecification;
+
+import pt.up.fe.specs.util.providers.ResourceProvider;
+
+/**
+ * @author Joao Bispo
+ *
+ */
+public enum LanguageSpecificationTestResource implements ResourceProvider {
+    ACTION_MODEL("actionModel.xml"),
+    ATTRIBUTE_MODEL("artifacts.xml"),
+    JOIN_POINT_MODEL("joinPointModel.xml");
+
+    private final String resource;
+
+    private static final String BASE_PACKAGE = "pt/up/fe/specs/lara/languagespecification/test/";
+
+    /**
+     * @param resource
+     */
+    private LanguageSpecificationTestResource(String resource) {
+        this.resource = BASE_PACKAGE + resource;
+    }
+
+    @Override
+    public String getResource() {
+        return resource;
+    }
+
+}
