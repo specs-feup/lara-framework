c11380b723e2644cd4f52278083a87f08bb57c70
João Bispo <joaobispo@gmail.com>
2020-05-11 03:20:34 +0100

    [LanguageSpecification] Adds new LangSpecV2 parser based on XML DOM

---
+++ LanguageSpecification/src/pt/up/fe/specs/lara/langspec/LangSpecsXmlParser.java
@@ -0,0 +1,288 @@
+/**
+ * Copyright 2020 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.lara.langspec;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.lara.language.specification.dsl.Action;
+import org.lara.language.specification.dsl.Attribute;
+import org.lara.language.specification.dsl.Declaration;
+import org.lara.language.specification.dsl.JoinPointClass;
+import org.lara.language.specification.dsl.LanguageSpecificationV2;
+import org.lara.language.specification.dsl.Parameter;
+import org.lara.language.specification.dsl.Select;
+import org.lara.language.specification.dsl.types.EnumDef;
+import org.lara.language.specification.dsl.types.EnumValue;
+import org.lara.language.specification.dsl.types.IType;
+import org.lara.language.specification.dsl.types.PrimitiveClasses;
+import org.lara.language.specification.dsl.types.TypeDef;
+
+import pt.up.fe.specs.util.collections.MultiMap;
+import pt.up.fe.specs.util.xml.XmlDocument;
+import pt.up.fe.specs.util.xml.XmlElement;
+
+public class LangSpecsXmlParser {
+
+    public static LanguageSpecificationV2 parse(InputStream joinPointModel, InputStream attributeModel,
+            InputStream actionModel) {
+
+        var joinPointModelNode = XmlDocument.newInstance(joinPointModel, SchemaResource.JOIN_POINT_SCHEMA.toStream());
+        var attributeModelNode = XmlDocument.newInstance(attributeModel, SchemaResource.ATTRIBUTE_SCHEMA.toStream());
+        var actionModelNode = XmlDocument.newInstance(actionModel, SchemaResource.ACTION_SCHEMA.toStream());
+
+        // Setup global JoinPointClass
+        LanguageSpecificationV2 langSpecV2 = new LanguageSpecificationV2();
+        JoinPointClass global = JoinPointClass.globalJoinPoint(langSpecV2);
+        langSpecV2.setGlobal(global);
+
+        // Initialize types (typedef, enums), to have access to available names
+        for (var type : attributeModelNode.getElementsByName("object")) {
+            var typeDef = new TypeDef(type.getAttribute("name"));
+            langSpecV2.add(typeDef);
+
+            setOptional(type.getAttribute("tooltip"), typeDef::setToolTip);
+        }
+
+        for (var type : attributeModelNode.getElementsByName("enum")) {
+            var enumDef = new EnumDef(type.getAttribute("name"));
+            langSpecV2.add(enumDef);
+
+            setOptional(type.getAttribute("tooltip"), enumDef::setToolTip);
+
+            List<EnumValue> valuesList = toEnumValues(type.getElementsByName("value"), langSpecV2);
+            enumDef.setValues(valuesList);
+        }
+
+        List<JoinPointClass> jps = new ArrayList<>();
+        for (var jpNode : joinPointModelNode.getElementsByName("joinpoint")) {
+            var jp = new JoinPointClass(jpNode.getAttribute("class"), langSpecV2);
+            setOptional(jpNode.getAttribute("tooltip"), jp::setToolTip);
+            jps.add(jp);
+        }
+        Collections.sort(jps);
+        jps.stream().forEach(langSpecV2::add);
+
+        var joinpoints = joinPointModelNode.getElementsByName("joinpoints").get(0);
+        langSpecV2.setRoot(joinpoints.getAttribute("root_class"));
+        setOptional(joinpoints.getAttribute("root_alias"), langSpecV2::setRootAlias);
+
+        // Map of actions according to class
+        MultiMap<String, XmlElement> joinPointActions = new MultiMap<>();
+        List<XmlElement> globalActions = new ArrayList<>();
+        for (var actionNode : actionModelNode.getElementsByName("action")) {
+            var classeNames = actionNode.getAttribute("class");
+
+            // Global actions do not have a class value, or its value is '*'
+            if (classeNames.isEmpty() || classeNames.equals("*")) {
+                globalActions.add(actionNode);
+                continue;
+            }
+
+            for (String className : classeNames.split(",")) {
+                joinPointActions.add(className.strip(), actionNode);
+            }
+        }
+
+        populateGlobal(joinPointModelNode, attributeModelNode, actionModelNode, langSpecV2, global, globalActions);
+
+        // Populate TypeDef
+        for (var typeNode : attributeModelNode.getElementsByName("object")) {
+            TypeDef typeDef = langSpecV2.getTypeDefs().get(typeNode.getAttribute("name"));
+            List<Attribute> attributesList = convertAttributes(typeNode.getElementsByName("attribute"), langSpecV2);
+            typeDef.setFields(attributesList);
+        }
+
+        for (var jpNode : joinPointModelNode.getElementsByName("joinpoint")) {
+            String jpClass = jpNode.getAttribute("class");
+
+            JoinPointClass jp = langSpecV2.getJoinPoint(jpClass);
+            String extendsType = jpNode.getAttribute("extends");
+            if (!extendsType.isEmpty()) {
+                jp.setExtend(langSpecV2.getJoinPoint(extendsType));
+            } else {
+                jp.setExtend(global);
+            }
+
+            // Add class attributes
+
+            List<XmlElement> attributeNodes = attributeModelNode.getElementsByName("artifact").stream()
+                    .filter(attribute -> attribute.getAttribute("class").equals(jpClass))
+                    .collect(Collectors.toList());
+
+            jp.setAttributes(convertAttributes(attributeNodes, langSpecV2));
+
+            // Add selects
+            jp.setSelects(convertSelects(langSpecV2, jpNode.getElementsByName("select")));
+
+            // Add actions
+            jp.setActions(convertActions(langSpecV2, joinPointActions.get(jpClass)));
+        }
+
+        // Add default global attributes (e.g., joinPointType, instanceOf)
+        addDefaultGlobalAttributes(langSpecV2);
+
+        // System.out.println("JP: " + joinPointModelNode);
+        // System.out.println("ATTR: " + attributeModelNode);
+        // System.out.println("ACTIONS: " + actionModelNode);
+
+        return langSpecV2;
+
+    }
+
+    private static void setOptional(String attributeValue, Consumer<String> setter) {
+        if (attributeValue.isBlank()) {
+            return;
+        }
+
+        setter.accept(attributeValue);
+    }
+
+    private static List<EnumValue> toEnumValues(List<XmlElement> enumValues, LanguageSpecificationV2 langSpecV2) {
+        List<EnumValue> attributes = new ArrayList<>();
+
+        for (var enumValue : enumValues) {
+            EnumValue newAttribute = new EnumValue(enumValue.getAttribute("name"), null);
+            setOptional(enumValue.getAttribute("string"), newAttribute::setString);
+            attributes.add(newAttribute);
+        }
+
+        Collections.sort(attributes);
+
+        return attributes;
+    }
+
+    private static void populateGlobal(XmlDocument jpModel, XmlDocument artifacts, XmlDocument actionModel,
+            LanguageSpecificationV2 langSpecV2, JoinPointClass global, List<XmlElement> globalActionNodes) {
+
+        /*
+        GlobalJoinPoints globalSelects = jpModel.getJoinPointList().getGlobal();
+        if (globalSelects != null) {
+            convertSelects(langSpecV2, globalSelects.getSelect())
+                    .forEach(global::add);
+            // global.setSelects();
+        }
+        */
+
+        var globalAttributes = artifacts.getElementByName("global");
+        if (globalAttributes != null) {
+            convertAttributes(globalAttributes.getElementsByName("attribute"), langSpecV2)
+                    .forEach(global::add);
+        }
+
+        // Global actions either do not have 'class' set, or is set to '*'
+        // List<XmlElement> globalActionNodes = actionModel.getElementsByName("action").stream()
+        // .filter(node -> node.getAttribute("class").isBlank() || node.getAttribute("class").equals("*"))
+        // .collect(Collectors.toList());
+
+        global.getActions().addAll(convertActions(langSpecV2, globalActionNodes));
+    }
+
+    private static List<Attribute> convertAttributes(List<XmlElement> attributeNodes,
+            LanguageSpecificationV2 langSpec) {
+
+        List<Attribute> attributes = new ArrayList<>();
+        for (var attributeNode : attributeNodes) {
+
+            Attribute newAttribute = getAttribute(attributeNode, langSpec);
+            attributes.add(newAttribute);
+
+        }
+
+        Collections.sort(attributes);
+
+        return attributes;
+    }
+
+    private static Attribute getAttribute(XmlElement attributeNode, LanguageSpecificationV2 langSpec) {
+        String type = getType(attributeNode);
+        Attribute newAttribute = new Attribute(langSpec.getType(type), attributeNode.getAttribute("name"));
+        setOptional(attributeNode.getAttribute("tooltip"), newAttribute::setToolTip);
+
+        var parameterNodes = attributeNode.getElementsByName("parameter");
+        for (var parameterNode : parameterNodes) {
+            newAttribute.addParameter(langSpec.getType(getType(parameterNode)),
+                    parameterNode.getAttribute("name"));
+        }
+
+        return newAttribute;
+    }
+
+    private static String getType(XmlElement node) {
+        // Default type is "void"
+        return node.getAttribute("type", "void");
+    }
+
+    private static List<Action> convertActions(LanguageSpecificationV2 langSpecV2,
+            List<XmlElement> actionNodes) {
+
+        List<Action> newActions = new ArrayList<>();
+        for (var action : actionNodes) {
+            var parameterNodes = action.getElementsByName("parameter");
+            List<Parameter> declarations = new ArrayList<>();
+            for (var param : parameterNodes) {
+                IType type = langSpecV2.getType(getType(param));
+                String defaultValue = param.getAttribute("default");
+                declarations.add(new Parameter(type, param.getAttribute("name"), defaultValue));
+            }
+
+            Action newAction = new Action(langSpecV2.getType(action.getAttribute("return", "void")),
+                    action.getAttribute("name"), declarations);
+            setOptional(action.getAttribute("tooltip"), newAction::setToolTip);
+            newActions.add(newAction);
+        }
+
+        Collections.sort(newActions);
+        return newActions;
+    }
+
+    private static List<Select> convertSelects(LanguageSpecificationV2 langSpecV2,
+            List<XmlElement> selectNodes) {
+
+        List<Select> selects = new ArrayList<>();
+
+        for (var selectNode : selectNodes) {
+            String selectClassName = selectNode.getAttribute("class");
+            JoinPointClass selectJP = langSpecV2.getJoinPoint(selectClassName);
+            String alias = selectNode.getAttribute("alias");
+            alias = alias.equals(selectClassName) ? "" : alias; // Is this necessary?
+            Select newSelect = new Select(selectJP, alias);
+            newSelect.setToolTip(selectNode.getAttribute("tooltip"));
+            selects.add(newSelect);
+        }
+
+        Collections.sort(selects);
+
+        return selects;
+    }
+
+    private static void addDefaultGlobalAttributes(LanguageSpecificationV2 langSpec) {
+        var joinPointType = new Attribute(PrimitiveClasses.STRING, "joinPointType");
+        joinPointType.setDefault(true);
+        joinPointType.setToolTip("a String with the type of the join point");
+        langSpec.getGlobal().add(joinPointType);
+
+        var instanceOf = new Attribute(PrimitiveClasses.BOOLEAN, "instanceOf",
+                Arrays.asList(new Declaration(PrimitiveClasses.STRING, "name")));
+        instanceOf.setDefault(true);
+        instanceOf.setToolTip("true if the current join point is an instance of the given type");
+        langSpec.getGlobal().add(instanceOf);
+
+    }
+}
