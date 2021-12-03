3b7f96284f42cbcd7d7f943bc9aca4097033d189
João Bispo <joaobispo@gmail.com>
2021-12-03 19:32:35 +0000

    [LaraDoc] Adds test for JS documentation generation support #7

---
+++ LaraDoc/resources/pt/up/fe/specs/lara/doc/docExample.js
@@ -0,0 +1,44 @@
+/**
+ * A top-level class
+ */
+ class ExampleClass {
+ 
+ 	/**
+ 	 * The ExampleClass constructor.
+ 	 *
+	 * @param {string} stringParam - A string parameter
+	 * @param {number} [optionalNumberParam=0] - An optional
+	 */
+ 	constructor() {
+ 	}
+ 	
+ 	/**
+ 	 * An instance method of example class
+ 	 *
+ 	 * @param {number} param1 
+	 * @param {string} param2
+ 	 */
+ 	instanceMethod(param1, param2) {
+ 	}
+ 	
+ 	instanceMethodWithoutComments(param1, param2, param3) {
+ 	}
+ 	
+ 	/**
+ 	 * A static property
+ 	 */
+ 	//static staticProperty = 'someValue';
+    
+    /**
+     * A static method of example class
+     */
+    static staticMethod() {
+    	
+  	}
+ }
+ 
+ /**
+  * A top-level function
+  */
+ function exampleFunction(param1, param2) {
+ }
---
+++ LaraDoc/test/pt/up/fe/specs/lara/doc/JsDocTester.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2021 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.lara.doc;
+
+import org.junit.Test;
+
+import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.util.SpecsLogs;
+import pt.up.fe.specs.util.SpecsSystem;
+
+public class JsDocTester {
+
+    @Test
+    public void test() {
+        SpecsSystem.programStandardInit();
+
+        var tempFolder = SpecsIo.getTempFolder("laradoc_test/src-lara");
+        SpecsLogs.info("Cleaning temporary folder " + tempFolder.getAbsolutePath());
+        SpecsIo.deleteFolderContents(tempFolder);
+
+        SpecsIo.resourceCopy("pt/up/fe/specs/lara/doc/docExample.js", tempFolder);
+        String[] args = { "--weaver", "org.lara.interpreter.weaver.defaultweaver.DefaultWeaver", "--output",
+                "./doctest", "--clean", "--exclude", "_", "--packages",
+                "{'Test API': ['" + tempFolder.getAbsolutePath().replace('\\', '/') + "']}" };
+        LaraDocLauncher.main(args);
+    }
+
+}
