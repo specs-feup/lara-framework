9ec05cc2d9d785d9e7cb0bfcb80b80557b82e714
hugomfandrade <hugo.mf.andrade@gmail.com>
2022-02-21 18:14:21 +0000

    javascript files for lara generated files (FunctionJp and super classes)

---
+++ LaraCommonLanguage/src-lara/weaver/jp/DeclJp.js
@@ -0,0 +1,45 @@
+// laraImport("weaver.jp.StmtJp")
+// import StmtJp from "./StmtJp";
+const StmtJp = require("./StmtJp");
+
+/**
+ * 
+ *
+ * @class
+ */
+class DeclJp extends StmtJp {
+
+	constructor(astNode) {
+		super(astNode);
+		
+		// JoinPoint Types
+		this._JP_TYPES = new Set();
+		_lara_dummy_ = this._JP_TYPES.add('decl');
+		_lara_dummy_ = this._JP_TYPES.add('stmt');
+		_lara_dummy_ = this._JP_TYPES.add('joinpoint');
+	}
+
+	instanceOf(joinPointType) {
+		return this._JP_TYPES.has(joinPointType);
+
+		/*
+		if(joinPointType === this.joinPointType) {
+			return true;
+		}
+	    
+		return (new StmtJp(this.astNode)).instanceOf(joinPointType);
+		*/
+	}
+
+	/**
+	 * @return an Iterator with the types of this join point.
+	 */
+	types(joinPointType) {
+		return this._JP_TYPES.values();
+	}
+	
+	get joinPointType() { return 'decl'; }
+
+}
+
+module.exports = DeclJp;
---
+++ LaraCommonLanguage/src-lara/weaver/jp/StmtJp.js
@@ -0,0 +1,43 @@
+// laraImport("weaver.jp.JoinPoint")
+// import JoinPoint from "./JoinPoint";
+const JoinPoint = require("./JoinPoint");
+
+/**
+ * 
+ *
+ * @class
+ */
+class StmtJp extends JoinPoint {
+
+	constructor(astNode) {
+		super(astNode);
+
+		// JoinPoint Types
+		this._JP_TYPES = new Set();
+		_lara_dummy_ = this._JP_TYPES.add('stmt');
+		_lara_dummy_ = this._JP_TYPES.add('joinpoint');
+	}
+
+	instanceOf(joinPointType) {
+		return this._JP_TYPES.has(joinPointType);
+
+		/*
+		if(joinPointType === this.joinPointType) {
+			return true;
+		}
+	    
+		return (new JoinPoint(this.astNode)).instanceOf(joinPointType);
+		*/
+	}
+
+	/**
+	 * @return an Iterator with the types of this join point.
+	 */
+	types(joinPointType) {
+		return this._JP_TYPES.values();
+	}
+	
+	get joinPointType() { return 'stmt'; }
+}
+
+module.exports = StmtJp;
