81d3a2f68fa4333a9791626c97ace28ccd142736
João Bispo <joaobispo@gmail.com>
2022-05-25 18:04:28 +0100

    [LARAI] Updates results of tests JsImport and ImportMultipleFiles #14

--- LARAI/resources/larai/test/weaver/JsImport.mjs
+++ LARAI/resources/larai/test/weaver/JsImport.mjs
@@ -1,3 +1,9 @@
 import "default_weaver_output/JsImportFile.js";
 
-println("Imported JsImport")
+println("Imported MjsImport")
+
+var JsImport = 0;
+
+laraImport("lara.Strings");
+
+println("Imported lara.Strings: " + (typeof Strings));
