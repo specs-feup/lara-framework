e00977be78ad2e8e722ba4d0abc832be6f6df1cf
João Bispo <joaobispo@gmail.com>
2022-06-20 16:01:29 +0100

    [LARAI] Adds action $workspace.report in default weaver

--- LARAI/src/org/lara/interpreter/weaver/defaultweaver/abstracts/joinpoints/AWorkspace.java
+++ LARAI/src/org/lara/interpreter/weaver/defaultweaver/abstracts/joinpoints/AWorkspace.java
@@ -2,6 +2,9 @@ package org.lara.interpreter.weaver.defaultweaver.abstracts.joinpoints;
 
 import java.util.List;
 import org.lara.interpreter.weaver.interf.SelectOp;
+import org.lara.interpreter.weaver.interf.events.Stage;
+import java.util.Optional;
+import org.lara.interpreter.exception.ActionException;
 import org.lara.interpreter.weaver.defaultweaver.abstracts.ADefaultWeaverJoinPoint;
 import org.lara.interpreter.weaver.interf.JoinPoint;
 
@@ -20,6 +23,30 @@ public abstract class AWorkspace extends ADefaultWeaverJoinPoint {
      */
     public List<? extends AFolder> selectFolder() {
         return select(org.lara.interpreter.weaver.defaultweaver.abstracts.joinpoints.AFolder.class, SelectOp.DESCENDANTS);
+    }
+
+    /**
+     * 
+     */
+    public void reportImpl() {
+        throw new UnsupportedOperationException(get_class()+": Action report not implemented ");
+    }
+
+    /**
+     * 
+     */
+    public final void report() {
+        try {
+        	if(hasListeners()) {
+        		eventTrigger().triggerAction(Stage.BEGIN, "report", this, Optional.empty());
+        	}
+        	this.reportImpl();
+        	if(hasListeners()) {
+        		eventTrigger().triggerAction(Stage.END, "report", this, Optional.empty());
+        	}
+        } catch(Exception e) {
+        	throw new ActionException(get_class(), "report", e);
+        }
     }
 
     /**
@@ -62,6 +89,7 @@ public abstract class AWorkspace extends ADefaultWeaverJoinPoint {
     @Override
     protected final void fillWithActions(List<String> actions) {
         super.fillWithActions(actions);
+        actions.add("void report()");
     }
 
     /**
