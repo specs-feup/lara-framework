59d74bcb2f6d32698a00228f259ba51e8a333921
João Bispo <joaobispo@gmail.com>
2022-07-12 21:13:40 +0100

    [LaraApi] Adds Graphs.isLeafNode()

--- LaraApi/src-lara/lara/graphs/Graphs.js
+++ LaraApi/src-lara/lara/graphs/Graphs.js
@@ -7,82 +7,104 @@ laraImport("lara.graphs.EdgeData");
  * Current implementation uses Cytoscape.js (https://js.cytoscape.org/)
  */
 class Graphs {
-	
-	static #isLibLoaded = false;
-	
-	/**
-	 * @param {Object} [config = {}] configuration for the graph, according to what Cytoscape accepts as configuration object
-	 */
-	static newGraph(config) {
-		// Ensure library is loaded
-		Graphs.loadLibrary();
+  static #isLibLoaded = false;
 
-		const _config = config ?? {};
-		
-		return cytoscape(_config);
-	}
+  /**
+   * @param {Object} [config = {}] configuration for the graph, according to what Cytoscape accepts as configuration object
+   */
+  static newGraph(config) {
+    // Ensure library is loaded
+    Graphs.loadLibrary();
 
+    const _config = config ?? {};
 
-	static loadLibrary() {
-		if(Graphs.#isLibLoaded) {
-			return;
-		}
-		
-		load("https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.21.1/cytoscape.min.js");
-		Graphs.#isLibLoaded = true;
-	}
-	
-	static addNode(graph, nodeData) {
-		let _nodeData = nodeData ?? {};
-		
-		// Check if NodeData
-		if(!(_nodeData instanceof NodeData)) {
-			_nodeData = Object.assign(new NodeData(), _nodeData);
-		}
-		
-		return graph.add({ group: 'nodes', data: _nodeData});
-	}
-	
-	static addEdge(graph, sourceNode, targetNode, edgeData) {
-		let _edgeData = edgeData ?? {};
+    return cytoscape(_config);
+  }
 
-		// Check if EdgeData
-		if(!(_edgeData instanceof EdgeData)) {
-			_edgeData = Object.assign(new EdgeData(), _edgeData);
-		}
-				
-		_edgeData.source = sourceNode.id();
-		_edgeData.target = targetNode.id();
-				
-		return graph.add({ group: 'edges', data: _edgeData});
-	}
-	
-	static toDot(graph) {
-		var dot = "digraph test {\n";
-		
-		// Declare nodes
-		for(const node of graph.nodes()) {
-			//println("Node: ")
-			//printlnObject(node.data());
-			dot += '"' + node.id() + '" [label="'+Graphs._sanitizeDotLabel(node.data().toString())+'" shape=box];\n'
-			//println("Id: " + node.id)
-		}
-		
-		for(const edge of graph.edges()) {
-			dot += '"' + edge.data().source + '" -> "' + edge.data().target + '" [label="'+Graphs._sanitizeDotLabel(edge.data().toString())+'"];\n'
-			//println("Edge: ")
-			//printlnObject(edge.data());
-			//println("Source: " + edge.data().source);
-			//println("Target: " + edge.data().target);
-		}
-		
-		dot += "}\n";
-		
-		return dot;
-	}
-	
-	static _sanitizeDotLabel(label) {
-		return label.replaceAll("\n", "\\l")
-					.replaceAll("\r", "");
-	}
+  static loadLibrary() {
+    if (Graphs.#isLibLoaded) {
+      return;
+    }
+
+    load(
+      "https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.21.1/cytoscape.min.js"
+    );
+    Graphs.#isLibLoaded = true;
+  }
+
+  static addNode(graph, nodeData) {
+    let _nodeData = nodeData ?? {};
+
+    // Check if NodeData
+    if (!(_nodeData instanceof NodeData)) {
+      _nodeData = Object.assign(new NodeData(), _nodeData);
+    }
+
+    return graph.add({ group: "nodes", data: _nodeData });
+  }
+
+  static addEdge(graph, sourceNode, targetNode, edgeData) {
+    let _edgeData = edgeData ?? {};
+
+    // Check if EdgeData
+    if (!(_edgeData instanceof EdgeData)) {
+      _edgeData = Object.assign(new EdgeData(), _edgeData);
+    }
+
+    _edgeData.source = sourceNode.id();
+    _edgeData.target = targetNode.id();
+
+    return graph.add({ group: "edges", data: _edgeData });
+  }
+
+  static toDot(graph) {
+    var dot = "digraph test {\n";
+
+    // Declare nodes
+    for (const node of graph.nodes()) {
+      //println("Node: ")
+      //printlnObject(node.data());
+      dot +=
+        '"' +
+        node.id() +
+        '" [label="' +
+        Graphs._sanitizeDotLabel(node.data().toString()) +
+        '" shape=box];\n';
+      //println("Id: " + node.id)
+    }
+
+    for (const edge of graph.edges()) {
+      dot +=
+        '"' +
+        edge.data().source +
+        '" -> "' +
+        edge.data().target +
+        '" [label="' +
+        Graphs._sanitizeDotLabel(edge.data().toString()) +
+        '"];\n';
+      //println("Edge: ")
+      //printlnObject(edge.data());
+      //println("Source: " + edge.data().source);
+      //println("Target: " + edge.data().target);
+    }
+
+    dot += "}\n";
+
+    return dot;
+  }
+
+  static _sanitizeDotLabel(label) {
+    return label.replaceAll("\n", "\\l").replaceAll("\r", "");
+  }
+
+  /**
+   *
+   * @param {node} node
+   * @param {boolean} [loopsAreLeafs = false]
+   * @returns true if the outdegree (number of edges with this node as source) is zero, false otherwise. By default, if a node has a connection to itself (loop) it is not considered a leaf
+   */
+  static isLeafNode(node, loopsAreLeafs = false) {
+    const includeLoops = !loopsAreLeafs;
+    return node.outdegree(includeLoops) === 0;
+  }
 }
