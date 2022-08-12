048d93ca0ffb29272b8b2ddb7776e9599b8b6fd4
João Bispo <joaobispo@gmail.com>
2022-08-12 17:53:57 +0100

    [LaraApi] Adapts lara.Collections from LARA to JS

--- LaraApi/src-lara/lara/Collections.js
+++ LaraApi/src-lara/lara/Collections.js
@@ -1,40 +1,32 @@
-import lara._JavaTypes;
-import lara.JavaInterop;
-
+laraImport("lara._JavaTypes");
+laraImport("lara.JavaInterop");
 
 /**
  *  Utility methods related to Collections.
  *
  * @class
  */
-var Collections = {};
+class Collections {
+  /**
+   * @param {Object[]|java.util.List} values - Values to sort in-place.
+   *
+   * @return the sorted collection
+   */
+  static sort(values) {
+    // If array
+    if (isArray(values)) {
+      //println("IS ARRAY");
+      values.sort();
+      return values;
+    }
 
+    // If Java List
+    if (JavaInterop.isList(values)) {
+      //println("IS LIST");
+      _JavaTypes.getCollections().sort(values);
+      return values;
+    }
 
-/**
- * @param {Object[]|java.util.List} values - Values to sort in-place.
- *
- * @return the sorted collection
- */
-Collections.sort = function(values) {
-	
-
-
-	// If array
-	if(isArray(values)) {
-		//println("IS ARRAY");	
-		values.sort();
-		return values;
-	}
-	
-	
-	// If Java List
-	if(JavaInterop.isList(values)) {
-		//println("IS LIST");
-		_JavaTypes.getCollections().sort(values);
-		return values;
-	}
-
-	
-	throw "Expected either an array or a Java List: " + values;
-	
+    throw "Expected either an array or a Java List: " + values;
+  }
 }
