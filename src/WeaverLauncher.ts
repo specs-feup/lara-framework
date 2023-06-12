250e55b4c7be91c1078b157c14e6a2eb5ffbf315
L. Sousa <lm.sousa@fe.up.pt>
2023-06-12 22:21:09 +0100

    Refactor code to make it more generic

--- src/WeaverLauncher.ts
+++ src/WeaverLauncher.ts
@@ -1,6 +1,8 @@
 import Debug from "debug";
+import yargs from "yargs";
 import * as path from "path";
 import * as chokidar from "chokidar";
+import { hideBin } from "yargs/helpers";
 import { fork } from "child_process";
 import {
   addActiveChildProcess,
@@ -10,64 +12,140 @@ import {
 
 listenForTerminationSignals();
 
-const debug = Debug("clava:main");
+export default class WeaverLauncher {
+  debug!: Debug.Debugger;
 
-function capitalizeFirstLetter(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+  weaverName!: string;
+  weaverPrettyName!: string;
+  weaverFileName!: string;
 
-export function main(args: Record<string, unknown>): void {
-  debug("Clava execution arguments: %O", args);
-  void executeClava(args);
+  commandLineArgumentsConfig!: yargs.Arguments;
 
-  if (args.watch) {
-    for (const directory of args.watch as string[]) {
-      debug(`Watching directory: ${directory}`);
-    }
-
-    chokidar
-      .watch(args.watch as string[], { ignoreInitial: true })
-      .on("all", (event: string, filepath: string) => {
-        try {
-          return filesystemEventHandler(event, filepath, args);
-        } catch (error) {
-          console.error(error);
-        }
-      });
-  }
-}
-
-function filesystemEventHandler(
-  event: string,
-  filepath: string,
-  args: Record<string, unknown>
-) {
-  debug(`Source file event: ${capitalizeFirstLetter(event)} '${filepath}'`);
-  void executeClava(args);
-}
-
-let midExecution = false;
-async function executeClava(args: Record<string, unknown>) {
-  if (midExecution) return;
-  midExecution = true;
-  const activeProcess = Object.values(activeChildProcesses)[0];
-
-  if (activeProcess?.exitCode === null) {
-    const promise = new Promise((resolve) => {
-      activeProcess.once("exit", (code) => {
-        resolve(code);
-      });
-    });
-    if (activeProcess.kill()) {
-      await promise;
-      debug("Killed active process");
-    } else {
-      throw new Error("Could not kill active process");
-    }
-  }
-
-  addActiveChildProcess(
-    fork(path.join("dist", "Clava.js"), [JSON.stringify(args)])
-  );
   midExecution = false;
+
+  constructor(
+    weaverName: string,
+    prettyWeaverName: string = WeaverLauncher.capitalizeFirstLetter(weaverName)
+  ) {
+    this.weaverName = weaverName;
+    this.weaverPrettyName = prettyWeaverName;
+    this.weaverFileName = `${prettyWeaverName}.js`;
+    this.debug = Debug(`WeaverLauncher:${prettyWeaverName}:main`);
+  }
+
+  async execute(): Promise<void> {
+    this.commandLineArgumentsConfig = await this.generateConfig().parse();
+  }
+
+  static capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  protected main(args: yargs.Arguments): void {
+    this.debug(`${this.weaverPrettyName} execution arguments: %O`, args);
+    void this.executeWeaver(args);
+
+    if (args.watch) {
+      for (const directory of args.watch as string[]) {
+        this.debug(`Watching directory: ${directory}`);
+      }
+
+      chokidar
+        .watch(args.watch as string[], { ignoreInitial: true })
+        .on("all", (event: string, filepath: string) => {
+          try {
+            return this.filesystemEventHandler(event, filepath, args);
+          } catch (error) {
+            console.error(error);
+          }
+        });
+    }
+  }
+
+  protected filesystemEventHandler(
+    event: string,
+    filepath: string,
+    args: yargs.Arguments
+  ) {
+    this.debug(
+      `Source file event: ${WeaverLauncher.capitalizeFirstLetter(
+        event
+      )} '${filepath}'`
+    );
+    void this.executeWeaver(args);
+  }
+
+  async executeWeaver(args: yargs.Arguments) {
+    if (this.midExecution) return;
+    this.midExecution = true;
+    const activeProcess = Object.values(activeChildProcesses)[0];
+
+    if (activeProcess?.exitCode === null) {
+      const promise = new Promise((resolve) => {
+        activeProcess.once("exit", (code) => {
+          resolve(code);
+        });
+      });
+      if (activeProcess.kill()) {
+        await promise;
+        this.debug("Killed active process");
+      } else {
+        throw new Error("Could not kill active process");
+      }
+    }
+
+    addActiveChildProcess(
+      fork(path.join("dist", this.weaverFileName), [JSON.stringify(args)])
+    );
+    this.midExecution = false;
+  }
+
+  protected generateConfig() {
+    return yargs(hideBin(process.argv))
+      .scriptName(this.weaverName)
+      .command({
+        command: "$0 [script-file]",
+        describe: `Execute a ${this.weaverPrettyName} script`,
+        builder: (yargs) => {
+          return yargs
+            .positional("script-file", {
+              describe: `Path to ${this.weaverPrettyName} script file`,
+              type: "string",
+            })
+            .option("c", {
+              alias: "config",
+              describe: "Path to JSON config file",
+              type: "string",
+              config: true,
+            })
+            .option("w", {
+              alias: "watch",
+              describe: "Watch the following directory for changes",
+              type: "array",
+              default: [],
+              defaultDescription: "none",
+            });
+        },
+        handler: (argv) => {
+          try {
+            console.log(`Executing ${this.weaverPrettyName} script...`);
+            void this.main(argv);
+          } catch (error) {
+            console.error(error);
+          }
+        },
+      })
+      .command({
+        command: "init",
+        describe: `Initialize a new ${this.weaverPrettyName} project`,
+        handler: () => {
+          // TODO: Implement
+          console.log(`Initializing new ${this.weaverPrettyName} project...`);
+        },
+      })
+      .help()
+      .showHelpOnFail(true)
+      .strict()
+      .pkgConf(this.weaverName);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,8 @@
 #!/usr/bin/env node
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import { main } from "./ClavaLauncher.js";
+import WeaverLauncher from "./WeaverLauncher.js";
 
 const moduleName = "clava";
-const prettyName = "Clava";
 
-const config = await yargs(hideBin(process.argv))
-  .scriptName(moduleName)
-  .command({
-    command: "$0 [script-file]",
-    describe: `Execute a ${prettyName} script`,
-    builder: (yargs) => {
-      return yargs
-        .positional("script-file", {
-          describe: `Path to ${prettyName} script file`,
-          type: "string",
-        })
-        .option("c", {
-          alias: "config",
-          describe: "Path to JSON config file",
-          type: "string",
-          config: true,
-        })
-        .option("w", {
-          alias: "watch",
-          describe: "Watch the following directory for changes",
-          type: "array",
-          default: [],
-          defaultDescription: "none",
-        });
-    },
-    handler: (argv) => {
-      try {
-        console.log(`Executing ${prettyName} script...`);
-        void main(argv);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  })
-  .command({
-    command: "init",
-    describe: `Initialize a new ${prettyName} project`,
-    handler: () => {
-      // TODO: Implement
-      console.log(`Initializing new ${prettyName} project...`);
-    },
-  })
-  .help()
-  .showHelpOnFail(true)
-  .strict()
-  .pkgConf(moduleName)
-  .parse();
+const weaver = new WeaverLauncher(moduleName);
+
+await weaver.execute();
