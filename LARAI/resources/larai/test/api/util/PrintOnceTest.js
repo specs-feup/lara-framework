8735aacb45a804ce15489d06bfcf28a47425b629
João Bispo <joaobispo@gmail.com>
2023-06-25 01:41:43 +0100

    [LARAI] Cleans PrintOnceTest.js

--- LARAI/resources/larai/test/api/util/PrintOnceTest.js
+++ LARAI/resources/larai/test/api/util/PrintOnceTest.js
@@ -1,24 +1,5 @@
 laraImport("lara.util.Replacer");
-/*
-let countBefore = 0;
-for (const key in globalThis) {
-  countBefore++;
-}
-println("Global this # keys before:" + countBefore);
-*/
 laraImport("lara.util.PrintOnce");
-
-// Antes desta chamada terminar, é feito o eval para fazer import e colocar no globalThis
-
-// ... mas uma chamada ao PrintOnce aqui dá erro, ainda não está no globalThis
-/*
-let countAfter = 0;
-for (const key in globalThis) {
-  countAfter++;
-}
-println("Global this # keys after:" + countAfter);
-*/
-// Só depois disto terminar é que coloca no globalThis
 
 PrintOnce.message("a");
 PrintOnce.message("b");
