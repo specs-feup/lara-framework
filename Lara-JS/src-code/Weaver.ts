55ccdafe8524dc6a84adb92bcfff23c4c3426ac2
L. Sousa <lm.sousa@fe.up.pt>
2023-07-02 15:13:47 +0100

    [Lara-JS] Major refactor of Weaver module

--- Lara-JS/src-code/Weaver.ts
+++ Lara-JS/src-code/Weaver.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import EventEmitter from "events";
 import java from "java";
 import Debug from "debug";
 import { fileURLToPath } from "url";
@@ -23,158 +24,212 @@ java.asyncOptions = {
   promisify: promisify,
 };
 
-function waitForMessage(): Promise<WeaverMessageFromLauncher> {
+export class Weaver {
+  static #isSetup = false;
+  static #javaWeaver: unknown;
+
+  static get isSetup() {
+    return Weaver.#isSetup;
+  }
+
+  static async awaitSetup() {
+    while (!Weaver.isSetup) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+  }
+
+  static async setupWeaver(
+    args: WeaverMessageFromLauncher["args"],
+    config: WeaverMessageFromLauncher["config"]
+  ) {
+    // Create debug instance
+    const debug = Debug(`Weaver:${config.weaverPrettyName}`);
+    debug("Initiating weaver setup.");
+
+    // Setup java
+    java.classpath.push(config.jarFilePath);
+    await java.ensureJvm();
+
+    debug(`${config.weaverPrettyName} execution arguments: %O`, args);
+
+    const javaWeaverClassName = config.javaWeaverQualifiedName.match(
+      new RegExp("(?<=\\.)\\w+$")
+    )?.[0];
+
+    if (javaWeaverClassName === undefined || javaWeaverClassName === null) {
+      throw new Error("Invalid weaver class name.");
+    }
+
+    /* eslint-disable */
+    // This code is intentionally ignored by eslint
+    const JavaArrayList = java.import("java.util.ArrayList");
+    const JavaFile = java.import("java.io.File");
+    const JavaLaraIDataStore = java.import(
+      "org.lara.interpreter.joptions.config.interpreter.LaraIDataStore"
+    );
+    const JavaDataStore = java.import(
+      "org.suikasoft.jOptions.Interfaces.DataStore"
+    );
+    const LaraiKeys = java.import(
+      "org.lara.interpreter.joptions.config.interpreter.LaraiKeys"
+    );
+    const NodeJsEngine = java.import("pt.up.fe.specs.jsengine.NodeJsEngine");
+    const JavaEventTrigger = java.import(
+      "org.lara.interpreter.weaver.events.EventTrigger"
+    );
+    const JavaWeaverClass = java.import(config.javaWeaverQualifiedName);
+
+    const fileList = new JavaArrayList();
+    //const [command, clangArgs, env] = await Sandbox.splitCommandArgsEnv(args._[1]);
+    const clangArgs = args._.slice(1);
+    clangArgs.forEach((arg: string | number) => {
+      fileList.add(new JavaFile(arg));
+    });
+
+    const javaWeaver = new JavaWeaverClass();
+    javaWeaver.setWeaver();
+    javaWeaver.setScriptEngine(new NodeJsEngine());
+    javaWeaver.setEventTrigger(new JavaEventTrigger());
+
+    const datastore = await new JavaDataStore.newInstanceP(
+      `${javaWeaverClassName}DataStore`
+    );
+    datastore.set(LaraiKeys.LARA_FILE, new JavaFile("placeholderFileName"));
+
+    const laraIDataStore = new JavaLaraIDataStore(null, datastore, javaWeaver);
+    javaWeaver.begin(
+      fileList,
+      new JavaFile(JavaWeaverClass.getWovenCodeFoldername()),
+      laraIDataStore.getWeaverArgs()
+    );
+    /* eslint-enable */
+
+    Object.defineProperty(globalThis, config.weaverName, {
+      value: new (class {
+        get rootJp() {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          return javaWeaver.getRootJp();
+        }
+        get weaver() {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          return javaWeaver;
+        }
+      })(),
+      enumerable: false,
+      configurable: true,
+      writable: false,
+    });
+
+    Weaver.#isSetup = true;
+    Weaver.#javaWeaver = javaWeaver;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    return debug;
+  }
+
+  static async executeScript(
+    args: WeaverMessageFromLauncher["args"],
+    config: WeaverMessageFromLauncher["config"],
+    debug: Debug.Debugger
+  ) {
+    debug("Executing user script...");
+    if (
+      typeof args.scriptFile === "string" &&
+      fs.existsSync(args.scriptFile) &&
+      isValidFileExtension(path.extname(args.scriptFile))
+    ) {
+      await import(path.resolve(args.scriptFile))
+        .then(() => {
+          debug("Execution completed successfully.");
+        })
+        .catch((error: JavaError) => {
+          console.error("Execution failed.");
+
+          if (error.cause !== undefined && error.cause !== null) {
+            // Java exception
+            console.error(error.cause.getMessage());
+          }
+          debug(error);
+        });
+    } else {
+      new Error("Invalid file path or file type.");
+    }
+  }
+
+  static shutdown() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
+    (Weaver.#javaWeaver as any).close();
+  }
+}
+
+/////////////////////////
+// import side effects //
+/////////////////////////
+
+/**
+ * Creates a promise that resolves to the message received from the event emitter.
+ * @param eventEmitter - The event emitter from which the message will be received.
+ * @returns A promise that resolves to the message received.
+ */
+function waitForMessage(
+  eventEmitter: EventEmitter
+): Promise<WeaverMessageFromLauncher> {
   return new Promise((resolve) => {
-    process.once("message", (message: WeaverMessageFromLauncher) => {
+    eventEmitter.once("message", (message: WeaverMessageFromLauncher) => {
       resolve(message);
     });
   });
 }
 
-if (directExecution) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const messageFromParent = await waitForMessage();
+/**
+ * This event emitter is used to receive conbfiguration data from the caller.
+ * If the weaver is being executed directly, the 'process' object is used instead.
+ *
+ * The event emitter could be the 'process' object in both cases but in the case of
+ * importing this file, it is a good idea to use a different object to avoid
+ * conflicts.
+ */
+const eventEmitter: EventEmitter = directExecution
+  ? process
+  : new EventEmitter();
 
-  const args = messageFromParent.args;
-  const config = messageFromParent.config;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { javaWeaver, debug } = await setupWeaver(
-    messageFromParent.args,
-    messageFromParent.config
-  );
-  await executeScript(args, config, debug);
+/**
+ * This function is used to send the configuration data to the weaver.
+ * @param message - Configuration data.
+ */
+export function setupWeaver(message: WeaverMessageFromLauncher) {
+  if (directExecution) {
+    throw new Error(
+      "Cannot run the setupWeaver function in direct execution mode."
+    );
+  }
+  eventEmitter.emit("message", message);
+}
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  javaWeaver.close();
-
-  debug("Exiting...");
-  process.exit(0);
-} else {
-  console.log("Not executing directly.");
-
-  if (Object.hasOwn(globalThis, "messageFromParent")) {
-    const messageFromParent = (
-      globalThis as unknown as { messageFromParent: WeaverMessageFromLauncher }
-    ).messageFromParent;
-
-    const { javaWeaver, debug } = await setupWeaver(
+/**
+ * This promise is used to wait for the configuration data to be received.
+ * The weaver is executed the configuration data is received.
+ */
+waitForMessage(eventEmitter)
+  .then(async (messageFromParent) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const debug = await Weaver.setupWeaver(
       messageFromParent.args,
       messageFromParent.config
     );
-  } else {
-    console.error("No configuration provided.");
-  }
-}
 
-export async function setupWeaver(
-  args: WeaverMessageFromLauncher["args"],
-  config: WeaverMessageFromLauncher["config"]
-) {
-  const debug = Debug(`Weaver:${config.weaverPrettyName}`);
-  debug("Starting weaver child process.");
+    if (directExecution) {
+      await Weaver.executeScript(
+        messageFromParent.args,
+        messageFromParent.config,
+        debug
+      );
 
-  //java.classpath.push("../../../.local/bin/Clava/Clava.jar");
-  java.classpath.push(config.jarFilePath);
-  await java.ensureJvm();
+      Weaver.shutdown();
 
-  debug(`${config.weaverPrettyName} execution arguments: %O`, args);
-
-  const javaWeaverClassName = config.javaWeaverQualifiedName.match(
-    new RegExp("(?<=\\.)\\w+$")
-  )?.[0];
-
-  if (javaWeaverClassName === undefined || javaWeaverClassName === null) {
-    throw new Error("Invalid weaver class name.");
-  }
-
-  /* eslint-disable */
-  // This code is intentionally ignored by eslint
-  const JavaArrayList = java.import("java.util.ArrayList");
-  const JavaFile = java.import("java.io.File");
-  const JavaLaraIDataStore = java.import(
-    "org.lara.interpreter.joptions.config.interpreter.LaraIDataStore"
-  );
-  const JavaDataStore = java.import(
-    "org.suikasoft.jOptions.Interfaces.DataStore"
-  );
-  const LaraiKeys = java.import(
-    "org.lara.interpreter.joptions.config.interpreter.LaraiKeys"
-  );
-  const NodeJsEngine = java.import("pt.up.fe.specs.jsengine.NodeJsEngine");
-  const JavaEventTrigger = java.import(
-    "org.lara.interpreter.weaver.events.EventTrigger"
-  );
-  const JavaWeaverClass = java.import(config.javaWeaverQualifiedName);
-
-  const fileList = new JavaArrayList();
-  //const [command, clangArgs, env] = await Sandbox.splitCommandArgsEnv(args._[1]);
-  const clangArgs = args._.slice(1);
-  clangArgs.forEach((arg: string | number) => {
-    fileList.add(new JavaFile(arg));
+      debug("Exiting...");
+      process.exit(0);
+    }
+  })
+  .catch((error) => {
+    console.error(error);
   });
-
-  const javaWeaver = new JavaWeaverClass();
-  javaWeaver.setWeaver();
-  javaWeaver.setScriptEngine(new NodeJsEngine());
-  javaWeaver.setEventTrigger(new JavaEventTrigger());
-
-  const datastore = await new JavaDataStore.newInstanceP(
-    `${javaWeaverClassName}DataStore`
-  );
-  datastore.set(LaraiKeys.LARA_FILE, new JavaFile("placeholderFileName"));
-
-  const laraIDataStore = new JavaLaraIDataStore(null, datastore, javaWeaver);
-  javaWeaver.begin(
-    fileList,
-    new JavaFile(JavaWeaverClass.getWovenCodeFoldername()),
-    laraIDataStore.getWeaverArgs()
-  );
-  /* eslint-enable */
-
-  Object.defineProperty(globalThis, config.weaverName, {
-    value: new (class {
-      get rootJp() {
-        return (javaWeaver as { getRootJp(): unknown }).getRootJp();
-      }
-      get javaWeaver() {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return javaWeaver;
-      }
-    })(),
-    enumerable: false,
-    configurable: true,
-    writable: false,
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  return { javaWeaver, debug };
-}
-
-export async function executeScript(
-  args: WeaverMessageFromLauncher["args"],
-  config: WeaverMessageFromLauncher["config"],
-  debug: Debug.Debugger
-) {
-  debug("Executing user script...");
-  if (
-    typeof args.scriptFile === "string" &&
-    fs.existsSync(args.scriptFile) &&
-    isValidFileExtension(path.extname(args.scriptFile))
-  ) {
-    await import(path.resolve(args.scriptFile))
-      .then(() => {
-        debug("Execution completed successfully.");
-      })
-      .catch((error: JavaError) => {
-        console.error("Execution failed.");
-
-        if (error.cause !== undefined && error.cause !== null) {
-          // Java exception
-          console.error(error.cause.getMessage());
-        }
-        debug(error);
-      });
-  } else {
-    console.error("Invalid file path or file type.");
-  }
-}
