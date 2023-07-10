0ef10ed80a71280438500b74d91a5c1d65c96388
L. Sousa <lm.sousa@fe.up.pt>
2023-07-11 00:06:00 +0100

    Fix Accumulator tests

--- Lara-JS/src-api/lara/util/Accumulator.test.ts
+++ Lara-JS/src-api/lara/util/Accumulator.test.ts
@@ -2,17 +2,16 @@ import Accumulator from "./Accumulator.js";
 
 describe("Accumulator", () => {
   describe("add", () => {
-    const acc = new Accumulator();
     it("should return size 1 when adding an element", () => {
+      const acc = new Accumulator();
       acc.add("step1");
       expect(acc.get("step1")).toBe(1);
     });
   });
 
   describe("add and get", () => {
-    const acc = new Accumulator();
-
     it("add the tuple (step1, step2)", () => {
+      const acc = new Accumulator();
       acc.add("step1", "step2");
 
       acc.add(["step1", "step2"]);
