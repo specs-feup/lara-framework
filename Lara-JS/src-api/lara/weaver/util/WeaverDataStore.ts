7cde763e5bbeeada0abde62984792c0227b642f9
L. Sousa <lm.sousa@fe.up.pt>
2023-07-10 15:51:45 +0100

    Refactor code from latest commit

--- Lara-JS/src-api/lara/weaver/util/WeaverDataStore.ts
+++ Lara-JS/src-api/lara/weaver/util/WeaverDataStore.ts
@@ -5,120 +5,103 @@ import Io from "../../Io.js";
 
 /**
  * DataStore used in LaraI weavers.
- * @constructor
  */
-export default class WeaverDataStore extends DataStore{
+export default class WeaverDataStore extends DataStore {
+  constructor(data: any, definition: any) {
+    data = data !== undefined ? data : "LaraI Options";
+    definition =
+      definition !== undefined
+        ? definition
+        : JavaTypes.getJavaLaraiKeys().STORE_DEFINITION;
+    super(data, definition);
+  }
 
-	constructor(data: any, definition: any){
-		data = data !== undefined ? data : "LaraI Options";
-		definition = definition !== undefined ? definition 
-		: JavaTypes.getLaraiKeys().STORE_DEFINITION;
-		super(data, definition);
-	}
+  /**
+   * Wraps a Java DataStore around a Lara DataStore.
+   */
+  protected dataStoreWrapper(javaDataStore: any) {
+    return new WeaverDataStore(javaDataStore, this.definition);
+  }
 
+  /**
+   * @returns a number between 0 and 3, representing the verbose level (0 - less information, 3 - more information)
+   */
+  getVerboseLevel() {
+    return this.get("verbose").ordinal();
+  }
 
-	/*** PRIVATE OVERRIDABLE FUNCTIONS ***/
+  /**
+   * @param verboseLevel - a number between 0 and 3, representing the verbose level (0 - less information, 3 - more information)
+   */
+  setVerboseLevel(verboseLevel: number) {
+    if (!(verboseLevel >= 0 && verboseLevel <= 3)) {
+      throw "WeaverDataStore.setVerboseLevel: expected a number  between 0 and 3";
+    }
 
-	/**
- 	* Wraps a Java DataStore around a Lara DataStore.
- 	*/
-	private dataStoreWrapper(javaDataStore: any) {
-		return new WeaverDataStore(javaDataStore, this.definition);
-	}
+    this.put("verbose", JavaTypes.getJavaVerboseLevel().values()[verboseLevel]);
+  }
 
+  /**
+   * @returns a java.io.File representing the current output folder of the weaver
+   */
+  getOutputFolder() {
+    return this.get("output");
+  }
 
-	/*** NEW WEAVER_DATA_STORE FUNCTIONS ***/
+  /**
+   * @param outputFolder - a java.io.File or a String, representing the current output folder of the weaver
+   *
+   */
+  setOutputFolder(outputFolder: any) {
+    const normalizedOutputFolder = Io.getPath(outputFolder);
+    this.put("output", normalizedOutputFolder);
+  }
 
+  /**
+   *
+   */
+  getSourceFolders() {
+    const sourcesFileList = this.get("workspace");
+    return Weaver.toJs(sourcesFileList.getFiles());
+  }
 
-	/**
- 	* @return a number between 0 and 3, representing the verbose level (0 - less information, 3 - more information)
- 	*/
-	getVerboseLevel() {
-		return this.get("verbose").ordinal();
-	}
+  /**
+   *
+   */
+  setSourceFolders(sourceFolders: any) {
+    const fileList = JavaTypes.getJavaFileList().newInstance(sourceFolders); // Change to JavaTypes
+    this.put("workspace", fileList);
+  }
 
-	/**
-	 * @param verboseLevel a number between 0 and 3, representing the verbose level (0 - less information, 3 - more information)
-	 */
-	setVerboseLevel(verboseLevel: number){
-		
-		if(!(verboseLevel >= 0 && verboseLevel <= 3)){
-			throw "WeaverDataStore.setVerboseLevel: expected a number  between 0 and 3";
-		}	
+  /**
+   *
+   */
+  getIncludeFolders() {
+    const includesFileList = this.get("include");
 
-		this.put("verbose", JavaTypes.getVerboseLevel().values()[verboseLevel]);
-	}
+    return Weaver.toJs(includesFileList.getFiles());
+  }
 
-	/**
- 	* @return a java.io.File representing the current output folder of the weaver
- 	*/
-	getOutputFolder() {
-		return this.get("output");
-	}
+  /**
+   *
+   */
+  setIncludeFolders(includeFolders: any) {
+    const fileList = JavaTypes.getJavaFileList().newInstance(includeFolders);
+    this.put("include", fileList);
+  }
 
-	/**
-	 * @param outputFolder a java.io.File or a String, representing the current output folder of the weaver
-	 * 
-	 */
-	setOutputFolder(outputFolder: any) {
-		const normalizedOutputFolder = Io.getPath(outputFolder);
-		this.put("output", normalizedOutputFolder);	
-	}
+  setTools(toolsPath: any) {
+    var toolsFile = JavaTypes.getJavaOptionalFile().newInstance(toolsPath);
+    this.put("tools", toolsFile);
+  }
 
-	/**
-	 * 
-	 */
-	getSourceFolders() {
-		const sourcesFileList = this.get("workspace");		
-		return Weaver.toJs(sourcesFileList.getFiles());
-	}
+  setLogFile(logPath: any) {
+    var logFile = JavaTypes.getJavaOptionalFile().newInstance(logPath);
+    this.put("log", logFile);
+  }
 
-	/**
-	 * 
-	 */
-	setSourceFolders(sourceFolders: any) {
-		
-		const fileList = JavaTypes.getFileList().newInstance(sourceFolders);  // Change to JavaTypes	
-		this.put("workspace", fileList);	
-	}
-
-
-	/**
-	 * 
-	 */
-	getIncludeFolders() {
-		const includesFileList = this.get("include");		
-
-		return Weaver.toJs(includesFileList.getFiles());
-	}
-
-	/**
-	 * 
-	 */
-	setIncludeFolders(includeFolders: any) {
-		
-		const fileList = JavaTypes.getFileList().newInstance(includeFolders);	
-		this.put("include", fileList);	
-	}
-
-	setTools(toolsPath: any) {
-		
-		var toolsFile = JavaTypes.getOptionalFile().newInstance(toolsPath);	
-		this.put("tools", toolsFile);	
-	}
-
-	setLogFile(logPath: any) {
-	
-		var logFile = JavaTypes.getOptionalFile().newInstance(logPath);	
-		this.put("log", logFile);	
-	}
-
-	getLogFile() {
-		
-		const optionalFile = this.get("log");
-		return optionalFile !== undefined ? optionalFile.getFile() : undefined;
-	
-	}
+  getLogFile() {
+    const optionalFile = this.get("log");
+    return optionalFile !== undefined ? optionalFile.getFile() : undefined;
+  }
 }
-
-		
