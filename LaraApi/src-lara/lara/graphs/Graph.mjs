1fe31e0402912d9eec5ef8380d5848291f0012c9
Milena Gouveia <up202008862@g.uporto.pt>
2023-07-17 13:45:30 +0100

    [LaraApi] Set "#graph" field as public in Graph class

--- LaraApi/src-lara/lara/graphs/Graph.mjs
+++ LaraApi/src-lara/lara/graphs/Graph.mjs
@@ -4,41 +4,41 @@ class Graph {
 	/**
 	 * A Cytoscape graph
 	 */
-	#graph;
+	graph;
 
 	/**
 	 * Creates a new instance of the Graph class
 	 * @param {Object} [config = {}] configuration for the graph, according to what Cytoscape accepts as configuration object
 	 */
 	constructor(config) {
-		this.#graph = Graphs.newGraph(config);
+		this.graph = Graphs.newGraph(config);
 	}
 
 	/**
 	 * Sets the Cytoscape graph
 	 */
 	set graph(graph) {
-		this.#graph = graph;
+		this.graph = graph;
 	}
 
 	/**
 	 * @returns the Cytoscape graph
 	 */
 	get graph() {
-		return this.#graph;
+		return this.graph;
 	}
 
 	/**
 	 * @returns the Cytoscape instance
 	 */
 	getCy() {
-		return this.#graph.cy();
+		return this.graph.cy();
 	}
 
 	/**
 	 * @param {lara.graphs.DotFormatter} dotFormatter 
 	 */
 	toDot(dotFormatter) {
-		return Graphs.toDot(this.#graph, dotFormatter);
+		return Graphs.toDot(this.graph, dotFormatter);
 	}
 }
