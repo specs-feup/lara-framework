847b1b6e724a7fa6bdb234a2e9c1fb712dc78f35
L. Sousa <lm.sousa@fe.up.pt>
2023-07-18 18:01:04 +0100

    Refactor core script importer

--- Lara-JS/src-api/core.ts
+++ Lara-JS/src-api/core.ts
@@ -1,18 +1,10 @@
-//import { coreFoo } from "core/coretest.js";
-
-// @ts-ignore
-//globalThis.coreFoo = coreFoo;
-//console.log("HEY")
-
-const prefix = "lara-js/api/core/"
+const prefix = "lara-js/api/core/";
 const coreImports = ["output.js"];
 
-// Test
-for(const coreImport of coreImports) {
-    const foo = Object.entries(await import(prefix+coreImport));
-    foo.forEach(([key, value]) => {
-        // @ts-ignore
-        globalThis[key] = value;
-    });
+for (const coreImport of coreImports) {
+  const foo = Object.entries(await import(prefix + coreImport));
+  foo.forEach(([key, value]) => {
+    // @ts-ignore
+    globalThis[key] = value;
+  });
 }
-
