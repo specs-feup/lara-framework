a2e5e8720b35b064d75a156d68da1b90fbc6bf28
Fabio Morais <fabiocunhamorais23@gmail.com>
2023-07-24 09:08:21 +0100

    Added Io test in jest mock in file creation required

---
+++ Lara-JS/src-api/lara/Io.test.ts
@@ -0,0 +1,32 @@
+import Io from "./Io.js";
+import JavaTypes, { JavaClasses } from "./util/JavaTypes.js";
+
+describe("Io", () => {
+
+    describe("getPaths with args", () => {
+        it("returns the list of text files in a folder when we pass '.txt' in args", () => {
+            
+            
+            //TODO - create mock to avoid file creation
+            
+            const testFolder = Io.mkdir("__ioTest__");
+            const testFile1= Io.writeFile(Io.getPath(testFolder, "test1.txt"), "test1");
+	        const testFile2= Io.writeFile(Io.getPath(testFolder, "test2.txt"), "test2");
+	        const testFile3= Io.writeFile(Io.getPath(testFolder, "test3.doc"), "test3");
+            const files: JavaClasses.File[] = Io.getPaths(testFolder, "*.txt");
+	        expect(files.map(file => file.getName()).sort().join()).toBe("test1.txt,test2.txt");	
+        
+        })
+    })
+    describe("getPaths without args", () => {
+        it("returns the list of files in the folder", () => {
+              
+            const testFolder = Io.mkdir("__ioTest__");
+	        const testFile1: JavaClasses.File = Io.writeFile(Io.getPath(testFolder, "test1.txt"), "test1");
+	        const testFile2: JavaClasses.File = Io.writeFile(Io.getPath(testFolder, "test2.txt"), "test2");
+	        const testFile3: JavaClasses.File = Io.writeFile(Io.getPath(testFolder, "test3.doc"), "test3");
+            const files: JavaClasses.File[] = Io.getPaths(testFolder);
+	        expect(files.map(file => file.getName()).sort().join()).toBe("test1.txt,test2.txt,test3.doc");	
+        })
+    })
+});
