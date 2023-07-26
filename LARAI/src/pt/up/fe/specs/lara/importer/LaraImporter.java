5c5605447fcebb678eefe27868e1cec0971b2562
L. Sousa <lm.sousa@fe.up.pt>
2023-07-26 13:44:31 +0100

    Fix importer file mismatch

--- LARAI/src/pt/up/fe/specs/lara/importer/LaraImporter.java
+++ LARAI/src/pt/up/fe/specs/lara/importer/LaraImporter.java
@@ -135,7 +135,7 @@ public class LaraImporter {
      */
     private Set<String> buildNpmImports() {
         return larai.getWeaverEngine().getNpmResources().stream()
-                .map(LaraResourceProvider::getResource)
+                .map(LaraResourceProvider::getFileLocation)
                 .collect(Collectors.toSet());
     }
 
