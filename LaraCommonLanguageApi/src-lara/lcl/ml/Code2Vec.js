ff626785fccf9ad55e3f90f2fe87a10e3c24536a
L. Sousa <lm.sousa@fe.up.pt>
2023-08-17 16:50:05 +0100

    Format file

--- LaraCommonLanguageApi/src-lara/lcl/ml/Code2Vec.js
+++ LaraCommonLanguageApi/src-lara/lcl/ml/Code2Vec.js
@@ -1,79 +1,55 @@
 laraImport("lcl.LaraCommonLanguage");
 laraImport("weaver.Query");
 
-
 class Code2Vec {
+  printPaths() {
+    // Collect information
+    for (var desc of Query.search("file").search()) {
+      if (!desc.hasChildren) {
+        //for all the leafs of the tree
+        this.#AST(desc.parent, [desc]);
+      }
+    }
+  }
 
-	//#currentPath;
+  #getNodeInfo(node) {
+    return node.joinPointType ?? node.toString();
+  }
 
-	constructor() {
-		//this.#currentPath = undefined;
-	}
+  #AST(node, lst) {
+    // return AST path context starting from a leaf
+    lst.push("(up)");
+    lst.push(node);
+    if (node.astParent != undefined && !lst.includes(node.astParent)) {
+      this.#AST(node.astParent, lst);
+    }
+    this.#goDown(node, lst);
+    lst.pop();
+    lst.pop();
+  }
 
-	printPaths() {
+  #goDown(parent, node_list) {
+    //go down in the ast to find the paths to the leafs
+    if (!parent.hasChildren) {
+      console.log(
+        ...node_list.map((node) => {
+          return `${this.#getNodeInfo(node)} `;
+        })
+      );
 
-		// Collect information 
-		for (var desc of Query.search("file").search()) {
-			if (!desc.hasChildren) { //for all the leafs of the tree
-				this.#AST(desc.parent, [desc]);
-			}
-		}
-	}
-
-
-
-	#getNodeInfo(node) {
-		var type = node.joinPointType;
-
-		if (type === undefined) {
-			type = node.toString();
-		}
-
-		return type;
-	}
-
-	#AST(node, lst) {                      // return AST path context starting from a leaf
-		lst.push('(up)');
-		lst.push(node);
-		if (node.astParent != undefined && !lst.includes(node.astParent)) {
-			this.#AST(node.astParent, lst);
-		}
-		this.#goDown(node, lst);
-		lst.pop();
-		lst.pop();
-	}
-
-
-	#goDown(parent, node_list) { //go down in the ast to find the paths to the leafs
-		if (!parent.hasChildren) {
-			for (var node of node_list) {
-				print(this.#getNodeInfo(node) + " ");
-				/*
-				if (node.code != undefined)
-					println(node.code);
-				else
-					println(node);
-				*/
-			}
-			println("\n__________________________________");
-			return;
-		}
-		else {
-			for (var node of parent.children) {
-				var previousNode = node_list[node_list.length - 3];
-				if (!(previousNode.equals(node))) {
-					node_list.push('(down)');
-					node_list.push(node);
-					this.#goDown(node, node_list);
-					node_list.pop();
-					node_list.pop();
-				}
-			}
-
-		}
-	}
-
+      console.log("__________________________________");
+      return;
+    } else {
+      for (var node of parent.children) {
+        var previousNode = node_list[node_list.length - 3];
+        if (!previousNode.equals(node)) {
+          node_list.push("(down)");
+          node_list.push(node);
+          this.#goDown(node, node_list);
+          node_list.pop();
+          node_list.pop();
+        }
+      }
+    }
+  }
 }
-
-
-
