1a126abade4a2cac54fe99d746bd54cbe54f1f03
L. Sousa <lm.sousa@fe.up.pt>
2023-09-04 00:33:26 +0100

    Refactor Debug and Numbers APIs

--- Lara-JS/src-api/lara/Debug.ts
+++ Lara-JS/src-api/lara/Debug.ts
@@ -1,35 +1,37 @@
 /**
- * @class
+ * @deprecated Not relevant anymore.
  */
-var Debug = {};
-
-/**
- * Collects the properties of an object that are of type function.
- * Taken from here: https://stackoverflow.com/questions/5842654/how-to-get-an-objects-methods
- * 
- * @param obj - An object 
- */
-Debug.getFunctions = function(obj)
-{
-    var res = [];
-    for(var m in obj) {
-        if(typeof obj[m] == "function") {
-            res.push(m);
-        }
+export default class Debug {
+  /**
+   * Collects the properties of an object that are of type function.
+   * Taken from here: https://stackoverflow.com/questions/5842654/how-to-get-an-objects-methods
+   *
+   * @param obj - An object
+   *
+   * @deprecated Use the recommendations in this post https://stackoverflow.com/questions/39544789/get-class-methods-in-typescript
+   */
+  static getFunctions(obj: any) {
+    const res = [];
+    for (const m in obj) {
+      if (typeof obj[m] == "function") {
+        res.push(m);
+      }
     }
     return res;
-}
+  }
 
-/**
- * Collects the properties of an object that are of type object.
- */
-Debug.getObjects = function(obj)
-{
-    var res = [];
-    for(var m in obj) {
-		 if(typeof obj[m] == "object") {
-            res.push(m);
-        }
+  /**
+   * Collects the properties of an object that are of type object.
+   *
+   * @deprecated Use the recommendations in this post https://stackoverflow.com/questions/39544789/get-class-methods-in-typescript
+   */
+  static getObjects(obj: any) {
+    const res = [];
+    for (const m in obj) {
+      if (typeof obj[m] == "object") {
+        res.push(m);
+      }
     }
     return res;
+  }
 }
--- Lara-JS/src-api/lara/Numbers.ts
+++ Lara-JS/src-api/lara/Numbers.ts
@@ -1,69 +1,52 @@
-/**
- * @class
- */
-var Numbers = {};
+export default class Numbers {
+  /**
+   * @param number - A number to convert to indexes.
+   * @returns An array with indexes corresponding to the active bits of the given number
+   */
+  static toIndexesArray(number: number) {
+    // Convert number to binary string
+    const binaryString = number.toString(2);
 
+    const indexesArray = [];
+    const numBits = binaryString.length;
+    for (let i = 0; i < numBits; i++) {
+      if (binaryString[i] === "0") {
+        continue;
+      }
 
+      indexesArray.push(numBits - i - 1);
+    }
 
-/**
- * @param {number} number - A number to convert to indexes.
- * @returns An array with indexes corresponding to the active bits of the given number
- */
-Numbers.toIndexesArray = function(number) {
-	checkNumber(number, "toIndexesArray");
+    return indexesArray.reverse();
+  }
 
-	// Convert number to binary string
-	var binaryString = number.toString(2);
-
-	var indexesArray = [];
-	var numBits = binaryString.length;
-	for(var i=0; i<numBits; i++) {
-		if(binaryString[i] === "0") {
-			continue;
-		}
-
-		indexesArray.push((numBits-i)-1);
-	}
-
-	return indexesArray.reverse();
-}
-
-/**
- * Taken from here: https://stackoverflow.com/questions/3959211/fast-factorial-function-in-javascript#3959275
- */ 
-Numbers.factorial = function(num) {
-    var rval=1;
-    for (var i = 2; i <= num; i++) {
-        rval = rval * i;
-	}
+  /**
+   * Taken from here: https://stackoverflow.com/questions/3959211/fast-factorial-function-in-javascript#3959275
+   */
+  static factorial(num: number): number {
+    let rval = 1;
+    for (let i = 2; i <= num; i++) {
+      rval *= i;
+    }
 
     return rval;
-}
+  }
 
-/**
- * @return {number} Arithmetic mean of the given values.
- */
-Numbers.mean = function(values) {
+  /**
+   * @deprecated THIS IS NOT A MEAN!!! THIS CALCULATES AN AVERAGE!!!.
+   */
+  static mean(values: number[]): number {
+    return Numbers.average(values);
+  }
 
-	var acc = 0;
-	var total = 0;
-	for(var value of values) {
-		acc += value;
-		total++;
-	}
-	
-	return acc / total;
-}
+  static average(values: number[]): number {
+    return values.reduce((acc, value) => acc + value, 0) / values.length;
+  }
 
-/**
- * @return {number} Sum of the given values.
- */
-Numbers.sum = function(values) {
-	
-	var acc = 0;
-	for(var value of values) {
-		acc += value;
-	}
-	
-	return acc;
+  /**
+   * @returns Sum of the given values.
+   */
+  static sum(values: number[]): number {
+    return values.reduce((acc, value) => acc + value, 0);
+  }
 }
