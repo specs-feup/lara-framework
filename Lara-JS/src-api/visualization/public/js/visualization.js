eafa60f0feb2909a1d4441d365c45b37a61094c3
Process-ing <rbruno.p.o@gmail.com>
2024-07-02 12:20:08 +0100

    Implement highlighting of nodes and code when code is hovered

--- Lara-JS/src-api/visualization/public/js/visualization.js
+++ Lara-JS/src-api/visualization/public/js/visualization.js
@@ -1,17 +1,24 @@
-const highlightNode = (node) => {
-  document.querySelectorAll(`span[data-node-id="${node.dataset.nodeId}"]`)
-    .forEach(element => element.classList.add('highlighted'));
+const getElementsWithNodeId = (id) => {
+  return document.querySelectorAll(`span[data-node-id="${id}"]`);
+}
+
+const highlightElements = (elements) => {
+  elements.forEach(element => element.classList.add('highlighted'));
 };
 
-const unhighlightNode = (node) => {
-  document.querySelectorAll(`span[data-node-id="${node.dataset.nodeId}"]`)
-    .forEach(element => element.classList.remove('highlighted'));
+const unhighlightElements = (elements) => {
+  elements.forEach(element => element.classList.remove('highlighted'));
 }
 
 const addEventListenersToAstNodes = (nodes) => {
   for (const nodeElement of nodes) {
-		nodeElement.addEventListener('mouseover', () => highlightNode(nodeElement));
-		nodeElement.addEventListener('mouseout', () => unhighlightNode(nodeElement));
+    const nodeId = nodeElement.dataset.nodeId;
+    const nodeRelatedElements = getElementsWithNodeId(nodeId);
+
+    for (const nodeRelatedElement of nodeRelatedElements) {
+      nodeRelatedElement.addEventListener('mouseover', () => highlightElements(nodeRelatedElements));
+      nodeRelatedElement.addEventListener('mouseout', () => unhighlightElements(nodeRelatedElements));
+    }
   }
 };
 
