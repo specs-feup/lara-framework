726bdc48105bae84da1526216b99513c0de7f103
Process-ing <rbruno.p.o@gmail.com>
2024-07-08 16:39:36 +0100

    Fix search and replace bounds

--- Lara-JS/src-api/visualization/public/js/utils.ts
+++ Lara-JS/src-api/visualization/public/js/utils.ts
@@ -8,4 +8,12 @@ const escapeHtml = (text: string): string => {
   return text.replace(/[&<>]/g, (match) => specialCharMap[match]);
 }
 
-export { escapeHtml };
+const replaceAfter = (text: string, search: string, replace: string, start: number): string => {
+  const index = text.indexOf(search, start);
+  if (index === -1) {
+    return text;
+  }
+  return text.slice(0, index) + replace + text.slice(index + search.length);
+}
+
+export { escapeHtml, replaceAfter };
