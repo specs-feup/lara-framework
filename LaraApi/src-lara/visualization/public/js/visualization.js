14e0d93419b9c5f4305a51799941d84431493b9a
Process-ing <rbruno.p.o@gmail.com>
2024-07-09 10:25:42 +0100

    Refactor highlighting

--- LaraApi/src-lara/visualization/public/js/visualization.js
+++ LaraApi/src-lara/visualization/public/js/visualization.js
@@ -2,14 +2,15 @@ const getElementsWithNodeId = (id) => {
     return document.querySelectorAll(`span[data-node-id="${id}"]`);
 };
 const highlightElements = (elements) => {
-    elements.forEach(element => element.classList.add('highlighted'));
+    elements.forEach(element => element.style.backgroundColor = 'yellow');
 };
 const unhighlightElements = (elements) => {
-    elements.forEach(element => element.classList.remove('highlighted'));
+    elements.forEach(element => element.style.backgroundColor = '');
 };
 const addEventListenersToAstNodes = (nodes) => {
     for (const nodeElement of nodes) {
         if (!nodeElement.dataset.nodeId) {
+            console.warn('Node element does not have a data-node-id attribute');
             continue;
         }
         const nodeId = nodeElement.dataset.nodeId;
