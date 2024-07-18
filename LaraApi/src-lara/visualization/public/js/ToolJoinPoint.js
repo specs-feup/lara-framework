f6ae29179ee560dfa508b6a1d5e2b3752fee5c77
Process-ing <rbruno.p.o@gmail.com>
2024-07-18 16:28:14 +0100

    Remove code from ToolJoinPoint

--- LaraApi/src-lara/visualization/public/js/ToolJoinPoint.js
+++ LaraApi/src-lara/visualization/public/js/ToolJoinPoint.js
@@ -1,27 +1,24 @@
 export default class ToolJoinPoint {
     id;
     type;
-    code;
     children;
-    constructor(id, type, code, children) {
+    constructor(id, type, children) {
         this.id = id;
         this.type = type;
-        this.code = code;
         this.children = children;
     }
     static fromJson(json) {
-        return new ToolJoinPoint(json.id, json.type, json.code, json.children.map((child) => ToolJoinPoint.fromJson(child)));
+        return new ToolJoinPoint(json.id, json.type, json.children.map((child) => ToolJoinPoint.fromJson(child)));
     }
     toJson() {
         return {
             id: this.id,
             type: this.type,
-            code: this.code,
             children: this.children.map(child => child.toJson()),
         };
     }
     clone() {
-        return new ToolJoinPoint(this.id, this.type, this.code, this.children.map(child => child.clone()));
+        return new ToolJoinPoint(this.id, this.type, this.children.map(child => child.clone()));
     }
 }
 ;
