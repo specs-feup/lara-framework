be0d21a3111de51368b347ba43ad77bb8fb5acf7
Process-ing <rbruno.p.o@gmail.com>
2024-07-19 13:28:34 +0100

    Refactor highlighting and remove dropdown from leaf nodes

--- LaraApi/src-lara/visualization/public/js/visualization.js
+++ LaraApi/src-lara/visualization/public/js/visualization.js
@@ -1,11 +1,8 @@
 const getNodeElement = (nodeId) => {
     return document.querySelector(`.ast-node[data-node-id="${nodeId}"]`);
 };
-const getNodeDropdown = (nodeId) => {
-    return document.querySelector(`.ast-node-dropdown[data-node-id="${nodeId}"]`);
-};
 const getNodeRelatedElements = (nodeId) => {
-    return Array.from(document.querySelectorAll(`.ast-node[data-node-id="${nodeId}"], .node-code[data-node-id="${nodeId}"]`));
+    return Array.from(document.querySelectorAll(`.ast-node[data-node-id="${nodeId}"] .ast-node-text, .node-code[data-node-id="${nodeId}"]`));
 };
 const highlightNode = (nodeId, strong) => {
     const nodeCode = document.querySelectorAll(`.node-code[data-node-id="${nodeId}"]`);
@@ -16,7 +13,7 @@ const highlightNode = (nodeId, strong) => {
     let parentNode = nodeElement.parentElement?.previousSibling;
     while (parentNode instanceof HTMLElement && parentNode.classList.contains('ast-node')) {
         const parentNodeText = parentNode.querySelector('.ast-node-text');
-        parentNodeText.style.backgroundColor = 'var(--secondary-highlight-color)';
+        parentNodeText.style.backgroundColor = strong ? 'var(--secondary-highlight-color)' : 'var(--tertiary-highlight-color)';
         parentNode = parentNode.parentElement?.previousSibling;
     }
 };
@@ -40,6 +37,8 @@ const addHighlighingEvents = (() => {
         for (const nodeRelatedElement of nodeRelatedElements) {
             nodeRelatedElement.addEventListener('mouseover', event => {
                 highlightNode(nodeId, false);
+                if (selectedNodeId !== null)
+                    highlightNode(selectedNodeId, true);
                 event.stopPropagation();
             });
             nodeRelatedElement.addEventListener('mouseout', event => {
@@ -49,15 +48,26 @@ const addHighlighingEvents = (() => {
                 event.stopPropagation();
             });
             nodeRelatedElement.tabIndex = 0;
-            nodeRelatedElement.role = "button";
             nodeRelatedElement.addEventListener('click', event => {
+                event.stopPropagation();
                 if (selectedNodeId !== null) {
                     unhighlightNode(selectedNodeId);
+                    if (selectedNodeId === nodeId) {
+                        selectedNodeId = null;
+                        return;
+                    }
                 }
                 selectedNodeId = nodeId;
                 highlightNode(nodeId, true);
-                for (const nodeRelatedElement of nodeRelatedElements.slice(0, 2)) {
-                    nodeRelatedElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                const nodeElement = getNodeElement(nodeId);
+                const firstNodeCodeBlock = document.querySelector('.node-code[data-node-id]');
+                for (const element of [nodeElement, firstNodeCodeBlock]) {
+                    element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                }
+            });
+            nodeRelatedElement.addEventListener('keydown', event => {
+                if (event.key === 'Enter') {
+                    nodeRelatedElement.click();
                 }
                 event.stopPropagation();
             });
@@ -66,17 +76,6 @@ const addHighlighingEvents = (() => {
 })();
 const addEventListenersToAstNodes = (root) => {
     const nodeId = root.id;
-    const nodeElement = getNodeElement(nodeId);
-    const nodeDropdownButton = nodeElement.children[0];
-    const nodeDropdown = getNodeDropdown(nodeId);
-    let nodeCollapsed = false;
-    nodeDropdownButton.addEventListener('click', event => {
-        nodeCollapsed = !nodeCollapsed;
-        nodeDropdown.style.display = nodeCollapsed ? 'none' : 'block';
-        const chevron = nodeDropdownButton.children[0];
-        chevron.textContent = nodeCollapsed ? 'keyboard_arrow_right' : 'keyboard_arrow_down';
-        event.stopPropagation();
-    });
     addHighlighingEvents(nodeId);
     root.children.forEach(child => addEventListenersToAstNodes(child));
 };
