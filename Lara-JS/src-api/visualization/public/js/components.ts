b281e0e771772c22d6ec13a9924ebdbc07736c13
Process-ing <rbruno.p.o@gmail.com>
2024-07-23 14:36:30 +0100

    Create components script

---
+++ Lara-JS/src-api/visualization/public/js/components.ts
@@ -0,0 +1,47 @@
+const getAstContainer = (() => {
+  const astContainer = document.querySelector<HTMLDivElement>('#ast-container');
+  if (!astContainer) {
+    throw new Error('Could not find AST container');
+  }
+  return (): HTMLDivElement => astContainer;
+})();
+
+const getCodeContainer = (() => {
+  const codeContainer = document.querySelector<HTMLDivElement>('#code-container');
+  if (!codeContainer) {
+    throw new Error('Could not find code container');
+  }
+  return (): HTMLDivElement => codeContainer;
+})();
+
+const getContinueButton = (() => {
+  const continueButton = document.querySelector<HTMLButtonElement>('#continue-button');
+  if (!continueButton) {
+    throw new Error('Could not find continue button');
+  }
+  return (): HTMLButtonElement => continueButton;
+})();
+
+const getResizer = (() => {
+  const resizer = document.querySelector<HTMLDivElement>('#resizer');
+  if (!resizer) {
+    throw new Error('Could not find resizer');
+  }
+  return (): HTMLDivElement => resizer;
+})();
+
+const getFileTabs = (() => {
+  const fileTabs = document.querySelector<HTMLDivElement>('#file-tabs');
+  if (!fileTabs) {
+    throw new Error('Could not find file tabs');
+  }
+  return (): HTMLDivElement => fileTabs;
+})();
+
+export {
+  getAstContainer,
+  getCodeContainer,
+  getContinueButton,
+  getResizer,
+  getFileTabs,
+};
