37aae5b7af98bc2a58f73c7c1b0f4074149b0d2f
Process-ing <rbruno.p.o@gmail.com>
2024-07-25 22:44:11 +0100

    Refactor GenericVisualizationTool

--- Lara-JS/src-api/visualization/GenericVisualizationTool.ts
+++ Lara-JS/src-api/visualization/GenericVisualizationTool.ts
@@ -4,39 +4,46 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import WebSocket, { WebSocketServer } from 'ws';
 import { AddressInfo } from 'net';
+import child from 'child_process';
 
-import { LaraJoinPoint, wrapJoinPoint } from '../LaraJoinPoint.js';
+import { LaraJoinPoint } from '../LaraJoinPoint.js';
 import JoinPoints from '../weaver/JoinPoints.js';
-import GenericAstConverter from './GenericAstConverter.js';
+import GenericAstConverter, { FilesCode } from './GenericAstConverter.js';
+import ToolJoinPoint from './public/js/ToolJoinPoint.js';
 
 
 export default abstract class GenericVisualizationTool {
-  private hostname: string | undefined;
-  private port: number | undefined;
-  private wss: WebSocketServer | undefined;
-  private serverClosed: boolean = false;
-  private astRoot: LaraJoinPoint | undefined;
+  #hostname: string | undefined;
+  #port: number | undefined;
+  #wss: WebSocketServer | undefined;
+  #serverClosed: boolean = false;
+  #toolAst: ToolJoinPoint | undefined;
+  #prettyHtmlCode: FilesCode | undefined;
 
   public isLaunched(): boolean {
-    return this.wss !== undefined && this.serverClosed === false;
+    return this.#wss !== undefined && this.#serverClosed === false;
   }
 
-  public getHostname(): string | undefined {
-    return this.hostname;
+  get hostname(): string | undefined {
+    return this.#hostname;
   }
 
-  public getPort(): number | undefined {
-    return this.port;
+  get port(): number | undefined {
+    return this.#port;
+  }
+
+  get url(): string | undefined {
+    return this.#hostname && this.#port ? `http://${this.#hostname}:${this.#port}` : undefined;
   }
 
   private onWssError(error: NodeJS.ErrnoException): void {
     switch (error.code) {
       case 'EADDRINUSE':
-        console.error(`[server]: Port ${this.port} is already in use`);
+        console.error(`[server]: Port ${this.#port} is already in use`);
         break;
 
       case 'EACCES':
-        console.error(`[server]: Permission denied to use port ${this.port}`);
+        console.error(`[server]: Permission denied to use port ${this.#port}`);
         break;
       
       default:
@@ -44,56 +51,53 @@ export default abstract class GenericVisualizationTool {
         break;
     };
 
-    this.wss!.close();
+    this.#wss!.close();
+  }
+
+  private updateAstAndCode(astRoot: LaraJoinPoint): void {
+    const astConverter = this.getAstConverter();
+    astConverter.updateAst();
+    
+    this.#toolAst = astConverter.getToolAst(astRoot);
+    this.#prettyHtmlCode = astConverter.getPrettyHtmlCode(astRoot);
+  }
+
+  private openBrowser(url: string): void {
+    const command = process.platform == 'darwin' ? 'open' : process.platform == 'win32'? 'start' : 'xdg-open';
+    child.exec(`${command} ${url}`);
   }
 
   public async launch(hostname: string = '127.0.0.1', port?: number, astRoot: LaraJoinPoint = JoinPoints.root()): Promise<void> {
     if (this.isLaunched()) {
-      console.warn(`Visualization tool is already running at http://${this.hostname}:${this.port}`);
+      console.warn(`Visualization tool is already running at ${this.url}`);
       return;
     }
 
-    this.astRoot = astRoot;
-
     const app = express();
     const server = http.createServer(app);
-    this.wss = new WebSocketServer({ server: server });
+    this.#wss = new WebSocketServer({ server: server });
 
     const filename = fileURLToPath(import.meta.url);
     const dirname = path.dirname(filename);
 
     app.use(express.static(path.join(dirname, 'public')));
+    
+    this.updateAstAndCode(astRoot);
+    this.#wss.on('connection', ws => this.updateClient(ws));
 
-    this.wss.on('connection', (ws: WebSocket) => {
-      console.log('[server]: Client connected');
-
-      ws.on('message', (message: string) => {
-        console.log(`[server]: Received message => ${message}`);
-      });
-
-      ws.addEventListener('close', () => {
-        console.log('[server]: Client disconnected');
-      });
-    });  // TODO: Remove this
-      
-    this.wss.on('connection', ws => this.updateClient(ws));
-
-    this.wss.on('close', () => {
-      this.serverClosed = true;
-    });
-
-    this.wss.on('error', error => this.onWssError(error));
+    this.#wss.on('close', () => { this.#serverClosed = true; });
+    this.#wss.on('error', error => this.onWssError(error));
 
     return new Promise(res => {
       server.listen(port ?? 0, hostname, () => {
         const addressInfo = server.address() as AddressInfo;
-        this.hostname = addressInfo.address;
-        this.port = addressInfo.port;
-        this.serverClosed = false;
+        this.#hostname = addressInfo.address;
+        this.#port = addressInfo.port;
+        this.#serverClosed = false;
 
-        console.log(`\nVisualization tool is running at http://${this.hostname}:${this.port}\n`);
-        // child.exec(`xdg-open http://${this.host}:${this.port}`);
-        // TODO: See if opening automatically is a good idea
+        console.log(`\nVisualization tool is running at ${this.url}\n`);
+        this.openBrowser(this.url!);
+        
         res();
       });
     });
@@ -104,7 +108,7 @@ export default abstract class GenericVisualizationTool {
   }
 
   private sendToAllClients(data: any): void {
-    this.wss!.clients.forEach(ws => this.sendToClient(ws, data));
+    this.#wss!.clients.forEach(ws => this.sendToClient(ws, data));
   }
 
   private verifyToolIsRunning(): void {
@@ -113,17 +117,23 @@ export default abstract class GenericVisualizationTool {
     }
   }
 
-  public async waitForTool(): Promise<void> {
-    this.verifyToolIsRunning();
+  private updateClient(ws: WebSocket): void {
+    this.sendToClient(ws, {
+      message: 'update',
+      ast: this.#toolAst!,
+      code: this.#prettyHtmlCode!,
+    });
+  }
 
+  private async waitForTool(): Promise<void> {
     return new Promise(res => {
       let placeClientOnWait: (ws: WebSocket) => void;
 
       const waitOnMessage = (message: string) => {
         const data = JSON.parse(message);
         if (data.message === 'continue') {
-          this.wss!.clients.forEach(ws => {
-            this.wss!.off('connection', placeClientOnWait);
+          this.#wss!.clients.forEach(ws => {
+            this.#wss!.off('connection', placeClientOnWait);
             ws.off('message', waitOnMessage);
           });
 
@@ -137,27 +147,22 @@ export default abstract class GenericVisualizationTool {
         this.sendToClient(ws, { message: 'wait' });
       }
 
-      this.wss!.clients.forEach(placeClientOnWait);
-      this.wss!.on('connection', placeClientOnWait);
+      this.#wss!.clients.forEach(placeClientOnWait);
+      this.#wss!.on('connection', placeClientOnWait);
     });
   }
 
-  private updateClient(ws: WebSocket): void {
-    const astConverter = this.getAstConverter();
-
-    astConverter.updateAst();
-    this.sendToClient(ws, {
-      message: 'update',
-      ast: astConverter.getToolAst(this.astRoot!).toJson(),
-      code: astConverter.getPrettyHtmlCode(this.astRoot!),
-    });
-  }
-
-  public update(astRoot: LaraJoinPoint = JoinPoints.root()): void {
+  public async visualize(astRoot: LaraJoinPoint = JoinPoints.root()): Promise<void> {
     this.verifyToolIsRunning();
 
-    this.astRoot = astRoot;
-    this.wss!.clients.forEach(ws => this.updateClient(ws));
+    const astConverter = this.getAstConverter();
+    astConverter.updateAst();
+    
+    this.updateAstAndCode(astRoot);
+    this.#wss!.clients.forEach(ws => this.updateClient(ws));
+
+    console.log(`\nVisualization tool is still running at${this.url}\n`);
+    await this.waitForTool();
   }
 
   protected abstract getAstConverter(): GenericAstConverter;
