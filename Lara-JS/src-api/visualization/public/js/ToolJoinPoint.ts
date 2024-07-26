a7869f032804a78bffbe9d7a7c95a1203963ed00
Process-ing <rbruno.p.o@gmail.com>
2024-07-26 10:41:33 +0100

    Write documentation for ToolJoinPoint.ts

--- Lara-JS/src-api/visualization/public/js/ToolJoinPoint.ts
+++ Lara-JS/src-api/visualization/public/js/ToolJoinPoint.ts
@@ -17,30 +17,51 @@ export default class ToolJoinPoint {
     this.#children = children;
   }
 
+  /**
+   * @brief Returns the join point ID.
+   */
   get id(): string {
     return this.#id;
   }
 
+  /**
+   * @brief Returns the type of join point.
+   */
   get type(): string {
     return this.#type;
   }
 
+  /**
+   * @brief Returns the code of the join point.
+   */
   get code(): string | undefined {
     return this.#code;
   }
 
+  /**
+   * @brief Returns the filepath of the file this join point belongs to.
+   */
   get filepath(): string | undefined {
     return this.#filepath;
   }
 
+  /**
+   * @brief Returns extra information about the join point.
+   */
   get info(): JoinPointInfo {
     return this.#info;
   }
 
+  /**
+   * @brief Returns the children of the join point.
+   */
   get children(): ToolJoinPoint[] {
     return this.#children
   }
 
+  /**
+   * @brief Creates a new ToolJoinPoint object from a JSON object.
+   */
   public static fromJson(json: any): ToolJoinPoint {
     return new ToolJoinPoint(
       json.id,
@@ -52,6 +73,9 @@ export default class ToolJoinPoint {
     );
   }
 
+  /**
+   * @brief Converts the ToolJoinPoint object to a JSON object.
+   */
   public toJson(): any {
     return {
       id: this.#id,
@@ -63,6 +87,9 @@ export default class ToolJoinPoint {
     };
   }
 
+  /**
+   * @brief Clones the join point.
+   */
   public clone(): ToolJoinPoint {
     return new ToolJoinPoint(
       this.#id,
