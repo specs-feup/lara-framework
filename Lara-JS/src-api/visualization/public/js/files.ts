6c8f4099cdfb444f2ddc8eaa5c307edcc814acda
Process-ing <rbruno.p.o@gmail.com>
2024-07-26 10:37:22 +0100

    Write documentation for files.ts

--- Lara-JS/src-api/visualization/public/js/files.ts
+++ Lara-JS/src-api/visualization/public/js/files.ts
@@ -1,8 +1,19 @@
+/**
+ * @file files.ts
+ * @brief Functions for handling files in the visualization.
+ */
+
 import { addFileCode, updateLines } from "./ast-import.js";
 import { createFileTab, getActiveCodeElement, getActiveFileTab, getFileCodeElement, getFileTab, getFileTabs, getMainCodeWrapper } from "./components.js";
 
 let selectedFilepath: string | null = null;
 
+/**
+ * @brief Adds a new file, with the respective file tab and (hidden) code, to the visualization.
+ * 
+ * @param path Path of the file
+ * @param code File code
+ */
 const addFile = (path: string, code: string): void => {
   addFileCode(code, path);
   
@@ -13,6 +24,9 @@ const addFile = (path: string, code: string): void => {
   fileTabs.appendChild(fileTab);
 };
 
+/**
+ * @brief Clears all files from the code container.
+ */
 const clearFiles = (): void => {
   const codeWrapper = getMainCodeWrapper();
   if (!codeWrapper)
@@ -25,7 +39,10 @@ const clearFiles = (): void => {
   selectedFilepath = null;
 };
 
-
+/**
+ * @brief Selects a file, by making its code visible in the code container.
+ * @param filepath 
+ */
 const selectFile = (filepath: string): void => {
   const fileTab = getFileTab(filepath);
   if (!fileTab)
