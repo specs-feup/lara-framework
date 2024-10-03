1ef9ed61c54e15277a548006f5c849a057404d3d
L. Sousa <lm.sousa@fe.up.pt>
2024-10-03 16:25:35 +0100

    Fix refactored test

--- LARAI/resources/larai/test/weaver/WeaverOptionsTest.js
+++ LARAI/resources/larai/test/weaver/WeaverOptionsTest.js
@@ -1,16 +1,14 @@
 //import WeaverOptions from "@specs-feup/lara/api/weaver/WeaverOptions.js";
 laraImport("weaver/WeaverOptions");
 
-/*export default*/ function WeaverData() {
-    const data = WeaverOptions.getData();
-    
-    console.log("Original verbose level: " + data.getVerboseLevel());
-    data.setVerboseLevel(3);
-    console.log("New verbose level: " + data.getVerboseLevel());
-    
-    console.log("Original output folder: " + data.getOutputFolder());
-    data.setOutputFolder("subfolder");
-    console.log("New output folder: " + data.getOutputFolder());
-    
-    console.log("Supported languages: " + WeaverOptions.getSupportedLanguages());
-}
+const data = WeaverOptions.getData();
+
+console.log("Original verbose level: " + data.getVerboseLevel());
+data.setVerboseLevel(3);
+console.log("New verbose level: " + data.getVerboseLevel());
+
+console.log("Original output folder: " + data.getOutputFolder());
+data.setOutputFolder("subfolder");
+console.log("New output folder: " + data.getOutputFolder());
+
+console.log("Supported languages: " + WeaverOptions.getSupportedLanguages());
--- LARAI/resources/larai/test/weaver/WeaverTest.js
+++ LARAI/resources/larai/test/weaver/WeaverTest.js
@@ -1,11 +1,9 @@
 //import Weaver from "@specs-feup/lara/api/weaver/Weaver.js";
 laraImport("weaver.Weaver");
 
-/*export default*/ function WeaverTest() {	
-    const ArrayList = Java.type('java.util.ArrayList');
-    const javaList = new ArrayList();
-    javaList.add(42);
-    javaList.add(13);	
-    const arrayFromList = Weaver.toJs(javaList);
-    console.log(arrayFromList.map(number => number + 1));
-}
+const ArrayList = Java.type('java.util.ArrayList');
+const javaList = new ArrayList();
+javaList.add(42);
+javaList.add(13);	
+const arrayFromList = Weaver.toJs(javaList);
+console.log(arrayFromList.map(number => number + 1));
