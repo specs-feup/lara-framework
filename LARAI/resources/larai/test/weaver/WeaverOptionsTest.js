66bfbf9b426d71e7380bf9bbfb297631683176aa
L. Sousa <lm.sousa@fe.up.pt>
2024-10-03 16:08:56 +0100

    Refector test

--- LARAI/resources/larai/test/weaver/WeaverOptionsTest.js
+++ LARAI/resources/larai/test/weaver/WeaverOptionsTest.js
@@ -1,23 +1,16 @@
-import weaver.WeaverOptions;
-import lara.Io;
+//import WeaverOptions from "@specs-feup/lara/api/weaver/WeaverOptions.js";
+laraImport("weaver/WeaverOptions");
 
-aspectdef WeaverData
-
-	var data = WeaverOptions.getData();
-
-	println("Original verbose level: " + data.getVerboseLevel());
-	data.setVerboseLevel(3);
-	println("New verbose level: " + data.getVerboseLevel());
-
-	println("Original output folder: " + data.getOutputFolder());
-	data.setOutputFolder("subfolder");
-	println("New output folder: " + data.getOutputFolder());
-	
-	println("Supported languages: " + WeaverOptions.getSupportedLanguages());
-	
-	// Cannot test this in a platform independent way
-	//println("CLI: " + WeaverOptions.toCli());
-	
-	//println("Weaver keys: " + WeaverOptions.getKeys());	
-
-end
+/*export default*/ function WeaverData() {
+    const data = WeaverOptions.getData();
+    
+    console.log("Original verbose level: " + data.getVerboseLevel());
+    data.setVerboseLevel(3);
+    console.log("New verbose level: " + data.getVerboseLevel());
+    
+    console.log("Original output folder: " + data.getOutputFolder());
+    data.setOutputFolder("subfolder");
+    console.log("New output folder: " + data.getOutputFolder());
+    
+    console.log("Supported languages: " + WeaverOptions.getSupportedLanguages());
+}
