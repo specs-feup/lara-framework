ae9a8639d148f8ab2066ea1c41e2d3ea073f232b
L. Sousa <lm.sousa@fe.up.pt>
2025-03-14 18:09:38 +0000

    Delete duplicate class

--- LARAI/src/org/lara/interpreter/utils/MessageConstants.java
+++ LARAI/src/org/lara/interpreter/utils/MessageConstants.java
@@ -12,8 +12,6 @@
  */
 package org.lara.interpreter.utils;
 
-import java.io.File;
-
 /**
  * Class containing all the predefined messages used on Lara
  * 
@@ -21,43 +19,26 @@ import java.io.File;
  * 
  */
 public class MessageConstants {
-	public static int order = 1;
-	public static final String USAGE = "usage: java -jar larai.jar <file.xml> [-<option> <optionParam>+] | -h"
-			+ "\noptions:" + "\n Option\t\tShort\tInput\t\tDescription" + "\n -help \t\t-h \t\t\tShows this message"
-			+ "\n -version  \t-v \t\t\tShows the version of the LARA interpreter"
-			+ "\n -javascript\t-js \t\t\tShow the resulting javascript in the console"
-			+ "\n -outdir  \t-od \t<path>\t\tChange Output directory of resulting files. Default: ." + File.separator
-			+ "\n -workdir \t-wd \t<path>\t\tChange Output working directory. Default: ." + File.separator
-			+ "\n -verbose \t-vb \t<level>\t\tChange the message level from 0(none) to 3(all). Default: 3" + "\n";
-	public static final String HOME_DIR = "." + File.separator;
-	public static final String PARENT_HOME_DIR = "." + MessageConstants.HOME_DIR;
-	public static final String OUT_DIR = MessageConstants.PARENT_HOME_DIR + "out" + File.separator;
-	public static final String WORK_DIR = MessageConstants.PARENT_HOME_DIR + "program" + File.separator;
-	public static final String DEFAULT_TARGET = "__DEFAULT__";
-	public static final String BRANCH_STR = "\\_ ";
+    public static final String OVERLINE = " -==================================================-";
+    public static final String UNDERLINE = MessageConstants.OVERLINE;
 
-	public static final String getElapsedTimeMessage(long timeMillis) {
-		return getElapsedTimeMessage(timeMillis, "Elapsed Time");
-	}
+    public static int order = 1;
 
-	public static final String getElapsedTimeMessage(long timeMillis, String text) {
-		StringBuilder sb = new StringBuilder(larac.utils.output.MessageConstants.UNDERLINE + "\n");
-		sb.append("  " + text + ": " + timeMillis + "ms\n");
-		sb.append(larac.utils.output.MessageConstants.OVERLINE + "");
-		return sb.toString();
-	}
+    public static final String getElapsedTimeMessage(long timeMillis) {
+        return getElapsedTimeMessage(timeMillis, "Elapsed Time");
+    }
 
-	public static final String getHeaderMessage(int order, String text) {
-		StringBuilder sb = new StringBuilder(larac.utils.output.MessageConstants.UNDERLINE + "\n");
-		sb.append("  " + order + ". " + text + "\n");
-		sb.append(larac.utils.output.MessageConstants.OVERLINE + "");
-		return sb.toString();
-	}
+    public static final String getElapsedTimeMessage(long timeMillis, String text) {
+        StringBuilder sb = new StringBuilder(MessageConstants.UNDERLINE + "\n");
+        sb.append("  " + text + ": " + timeMillis + "ms\n");
+        sb.append(MessageConstants.OVERLINE + "");
+        return sb.toString();
+    }
 
-	public static final String getMessage(String text) {
-		StringBuilder sb = new StringBuilder(larac.utils.output.MessageConstants.UNDERLINE + "\n");
-		sb.append("   " + text + "\n");
-		sb.append(larac.utils.output.MessageConstants.OVERLINE + "");
-		return sb.toString();
-	}
+    public static final String getHeaderMessage(int order, String text) {
+        StringBuilder sb = new StringBuilder(MessageConstants.UNDERLINE + "\n");
+        sb.append("  " + order + ". " + text + "\n");
+        sb.append(MessageConstants.OVERLINE + "");
+        return sb.toString();
+    }
 }
