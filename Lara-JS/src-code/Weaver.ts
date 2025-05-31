5abad5e1e9806c773391493dbb03a0e250dd4b45
L. Sousa <lm.sousa@fe.up.pt>
2025-05-31 16:51:00 +0100

    Add support for Node 22.x

--- Lara-JS/src-code/Weaver.ts
+++ Lara-JS/src-code/Weaver.ts
@@ -5,7 +5,6 @@ import java from "java";
 import Debug from "debug";
 import { fileURLToPath, pathToFileURL } from "url";
 import { isJavaError } from "./JavaError.js";
-import { promisify } from "util";
 import { isValidFileExtension } from "./FileExtensions.js";
 import WeaverMessageFromLauncher from "./WeaverMessageFromLauncher.js";
 import assert from "assert";
@@ -21,8 +20,7 @@ if (fileURLToPath(import.meta.url) === process.argv[1]) {
 java.asyncOptions = {
   asyncSuffix: "Async",
   syncSuffix: "",
-  promiseSuffix: "P",
-  promisify: promisify,
+  promiseSuffix: "P"
 };
 
 export class Weaver {
