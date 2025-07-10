a36d9927e59f09f587c8729b4159995629c0ac1a
João Bispo <jbispo@fe.up.pt>
2025-07-10 22:16:34 +0100

    [Lara-JS] Adds jestHelpers.registerSourceCodes(), which allows to load more than one file

--- Lara-JS/jest/jestHelpers.ts
+++ Lara-JS/jest/jestHelpers.ts
@@ -7,11 +7,24 @@ import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js";
  * @param code - String containing the source code.
  */
 export function registerSourceCode(code: string): void {
+  registerSourceCodes({ "dummyFile.cpp": code });
+}
+
+/**
+ *  Registers the source code that will be used by the weaver for the purpose of this test suite.
+ *
+ * @param codes - Record that maps file names to the corresponding source code.
+ */
+export function registerSourceCodes(codes: Record<string, string>): void {
   beforeAll(() => {
     Clava.getProgram().push();
     const program = Clava.getProgram();
-    const sourceFile = ClavaJoinPoints.fileWithSource("dummyFile.cpp", code);
-    program.addFile(sourceFile);
+
+    for (const key in codes) {
+      const sourceFile = ClavaJoinPoints.fileWithSource(key, codes[key]);
+      program.addFile(sourceFile);
+    }
+
     program.rebuild();
   });
 
