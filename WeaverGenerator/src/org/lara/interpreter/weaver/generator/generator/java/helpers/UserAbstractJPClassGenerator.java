d923742b05540d318a4fbc9af3e90c3740d8647c
João Bispo <jbispo@fe.up.pt>
2025-07-17 14:14:50 +0100

    [WeaverGenerator] Adds import in code generated from UserAbstractJPClassGenerator

--- WeaverGenerator/src/org/lara/interpreter/weaver/generator/generator/java/helpers/UserAbstractJPClassGenerator.java
+++ WeaverGenerator/src/org/lara/interpreter/weaver/generator/generator/java/helpers/UserAbstractJPClassGenerator.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2015 SPeCS.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -23,10 +23,11 @@ import org.specs.generators.java.enums.JDocTag;
 import org.specs.generators.java.enums.Modifier;
 import org.specs.generators.java.members.Method;
 
+import java.util.List;
+
 /**
  * Generates an abstract class that can be edited by the developer. This class can be used for changes/extensions that
  * are global to the join points.
- *
  */
 public class UserAbstractJPClassGenerator extends GeneratorHelper {
 
@@ -42,7 +43,7 @@ public class UserAbstractJPClassGenerator extends GeneratorHelper {
     /**
      * Generate an abstract class that can be edited by the developer. This class can be used for changes/extensions
      * that are global to the join points
-     * 
+     *
      * @param javaGenerator
      * @param sanitizedOutPackage
      * @param enums
@@ -56,7 +57,7 @@ public class UserAbstractJPClassGenerator extends GeneratorHelper {
     /**
      * Generate an abstract class that can be edited by the developer. This class can be used for changes/extensions
      * that are global to the join points
-     * 
+     *
      * @return
      */
     private JavaClass generateUserClass() {
@@ -82,6 +83,7 @@ public class UserAbstractJPClassGenerator extends GeneratorHelper {
         abstJPClass.add(GeneratorUtils.generateSelectGeneric(javaGenerator.getAJoinPointClass()));
         // abstJPClass.add(GeneratorUtils.generateSelectGeneric(abstJPClass));
         abstJPClass.addImport(SelectOp.class);
+        abstJPClass.addImport(List.class);
 
         // abstJPClass.add(method)
         return abstJPClass;
