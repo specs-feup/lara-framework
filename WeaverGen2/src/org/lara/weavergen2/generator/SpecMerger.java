3ae27cd9347d73dd522f612ab62f71c70038a4ed
L. Sousa <lm.sousa@fe.up.pt>
2026-03-20 18:06:52 +0000

    feat: Enhance SpecMerger by implementing attribute and action merging with duplicate checks

--- WeaverGen2/src/org/lara/weavergen2/generator/SpecMerger.java
+++ WeaverGen2/src/org/lara/weavergen2/generator/SpecMerger.java
@@ -2,6 +2,12 @@ package org.lara.weavergen2.generator;
 
 import org.lara.langspec2.model.*;
 import org.lara.langspec2.dsl.WeaverSpec;
+import org.lara.langspec2.validation.SpecValidationException;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Merges a base spec (from WeaverInterface) with a weaver-specific spec.
@@ -26,24 +32,22 @@ public final class SpecMerger {
         var weaverGlobal = weaverModel.getGlobal();
         var baseGlobal = baseModel.getGlobal();
 
-        // Prepend base attributes (they should come before weaver-specific ones)
-        var baseAttrs = baseGlobal.getOwnAttributes();
-        var existingAttrs = new java.util.ArrayList<>(weaverGlobal.getOwnAttributes());
+        var mergeErrors = new ArrayList<String>();
+        var mergedAttributes = mergeAttributes(baseGlobal.getOwnAttributes(), weaverGlobal.getOwnAttributes(), mergeErrors);
+        var mergedActions = mergeActions(baseGlobal.getOwnActions(), weaverGlobal.getOwnActions(), mergeErrors);
+
+        if (!mergeErrors.isEmpty()) {
+            throw new SpecValidationException(mergeErrors);
+        }
 
         // We need to clear and re-add in order to prepend
         // Since JpClass uses an internal mutable list, we must work around it
         // by creating a merged model manually.
         var mergedGlobal = new JpClass("joinpoint");
-        for (var attr : baseAttrs) {
+        for (var attr : mergedAttributes) {
             mergedGlobal.addAttribute(attr);
         }
-        for (var attr : existingAttrs) {
-            mergedGlobal.addAttribute(attr);
-        }
-        for (var action : baseGlobal.getOwnActions()) {
-            mergedGlobal.addAction(action);
-        }
-        for (var action : weaverGlobal.getOwnActions()) {
+        for (var action : mergedActions) {
             mergedGlobal.addAction(action);
         }
         if (weaverGlobal.getDefaultAttribute().isPresent()) {
@@ -88,5 +92,76 @@ public final class SpecMerger {
         weaverModel.getRoot().ifPresent(merged::setRoot);
 
         return merged;
+    }
+
+    private static List<Attribute> mergeAttributes(List<Attribute> baseAttrs, List<Attribute> weaverAttrs, List<String> errors) {
+        var mergedByName = new LinkedHashMap<String, Attribute>();
+        for (var attr : baseAttrs) {
+            mergedByName.put(attr.name(), attr);
+        }
+
+        for (var weaverAttr : weaverAttrs) {
+            var baseAttr = mergedByName.get(weaverAttr.name());
+            if (baseAttr == null) {
+                mergedByName.put(weaverAttr.name(), weaverAttr);
+                continue;
+            }
+
+            var effectiveTooltip = weaverAttr.tooltip() != null ? weaverAttr.tooltip() : baseAttr.tooltip();
+            var mergedAttr = new Attribute(weaverAttr.name(), weaverAttr.type(), weaverAttr.parameters(), effectiveTooltip);
+
+            if (isTrueDuplicate(baseAttr, mergedAttr)) {
+                errors.add("Duplicate attribute '" + weaverAttr.name() + "' in join point 'joinpoint'");
+                continue;
+            }
+
+            mergedByName.put(weaverAttr.name(), mergedAttr);
+        }
+
+        return List.copyOf(mergedByName.values());
+    }
+
+    private static List<Action> mergeActions(List<Action> baseActions, List<Action> weaverActions, List<String> errors) {
+        var mergedByName = new LinkedHashMap<String, Action>();
+        for (var action : baseActions) {
+            mergedByName.put(action.name(), action);
+        }
+
+        for (var weaverAction : weaverActions) {
+            var baseAction = mergedByName.get(weaverAction.name());
+            if (baseAction == null) {
+                mergedByName.put(weaverAction.name(), weaverAction);
+                continue;
+            }
+
+            var effectiveTooltip = weaverAction.tooltip() != null ? weaverAction.tooltip() : baseAction.tooltip();
+            var mergedAction = new Action(
+                    weaverAction.name(),
+                    weaverAction.returnType(),
+                    weaverAction.parameters(),
+                    effectiveTooltip
+            );
+
+            if (isTrueDuplicate(baseAction, mergedAction)) {
+                errors.add("Duplicate action '" + weaverAction.name() + "' in join point 'joinpoint'");
+                continue;
+            }
+
+            mergedByName.put(weaverAction.name(), mergedAction);
+        }
+
+        return List.copyOf(mergedByName.values());
+    }
+
+    private static boolean isTrueDuplicate(Attribute baseAttr, Attribute mergedAttr) {
+        return Objects.equals(baseAttr.type(), mergedAttr.type())
+                && Objects.equals(baseAttr.parameters(), mergedAttr.parameters())
+                && Objects.equals(baseAttr.tooltip(), mergedAttr.tooltip());
+    }
+
+    private static boolean isTrueDuplicate(Action baseAction, Action mergedAction) {
+        return Objects.equals(baseAction.returnType(), mergedAction.returnType())
+                && Objects.equals(baseAction.parameters(), mergedAction.parameters())
+                && Objects.equals(baseAction.tooltip(), mergedAction.tooltip());
     }
 }
