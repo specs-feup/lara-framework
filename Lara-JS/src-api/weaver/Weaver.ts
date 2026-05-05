38c1d31df6afc8670bb73a0790898939830273c9
L. Sousa <lm.sousa@fe.up.pt>
2026-05-05 01:00:50 +0100

    refactor: Remove deprecated methods and unused language specification in WeaverEngine

--- Lara-JS/src-api/weaver/Weaver.ts
+++ Lara-JS/src-api/weaver/Weaver.ts
@@ -130,26 +130,6 @@ export default class Weaver {
   }
 
   /**
-   * @param jpTypeName - a join point, or the name of a join point
-   * @param attributeName - the name of the attribute to check
-   *
-   * @returns True, if the given join point or join point name support the attribute with the given name
-   *
-   * @deprecated The typescript compiler will tell you this
-   */
-  static hasAttribute(jpTypeName: string, attributeName: string): boolean {
-    const joinPoint: any = Weaver.getWeaverEngine()
-      .getLanguageSpecificationV2()
-      .getJoinPoint(jpTypeName);
-
-    if (joinPoint === null) {
-      return false;
-    }
-
-    return !joinPoint.getAttribute(attributeName).isEmpty();
-  }
-
-  /**
    * @deprecated Should not be used. Here for a short period to allow for code migration.
    * 
    * Adapts a Java object to JavaScript. Currently converts:
