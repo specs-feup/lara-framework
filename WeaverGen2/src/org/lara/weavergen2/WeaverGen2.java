43beb5658ae24be98d110c61779fcea1e49a28a1
L. Sousa <lm.sousa@fe.up.pt>
2026-05-12 16:49:51 +0100

    fix WeaverGen2 JSON base joinpoint specialization
    Specialize base joinpoint references when emitting weaver JSON so inherited LaraJoinPoint members are represented with the concrete weaver root joinpoint type.
    
    This lets downstream TypeScript wrapper generation narrow inherited joinpoint-returning APIs without duplicating Weaver-side implementations.

--- WeaverGen2/src/org/lara/weavergen2/WeaverGen2.java
+++ WeaverGen2/src/org/lara/weavergen2/WeaverGen2.java
@@ -3,7 +3,6 @@ package org.lara.weavergen2;
 import org.lara.langspec2.dsl.WeaverSpec;
 import org.lara.langspec2.json.JsonSerializer;
 import org.lara.langspec2.model.*;
-import org.lara.langspec2.validation.SpecValidator;
 import org.lara.weavergen2.generator.*;
 import org.lara.weavergen2.java.TypeMapper;
 
@@ -15,26 +14,28 @@ import java.util.*;
  * Main entry point for the WeaverGen2 code generator.
  * <p>
  * Generates abstract join point classes, weaver abstract,
- * entities/enums, JSON spec, and DOT hierarchy diagram from a weaver specification.
+ * entities/enums, JSON spec, and DOT hierarchy diagram from a weaver
+ * specification.
  */
 public final class WeaverGen2 {
 
     private final WeaverModel model;
+    private final WeaverModel mergedModel;
     private final GeneratorConfig config;
 
-    public WeaverGen2(WeaverModel model, GeneratorConfig config) {
+    public WeaverGen2(WeaverModel model, WeaverModel mergedModel, GeneratorConfig config) {
         this.model = model;
+        this.mergedModel = mergedModel;
         this.config = config;
     }
 
     /**
      * Creates a generator from a base spec and weaver spec.
-     * <p>
-     * The base spec is only used to record inherited base signatures so the generator can
-     * recognize overrides in the weaver spec. It is not merged into the generated model.
      */
     public static WeaverGen2 fromSpecs(WeaverSpec baseSpec, WeaverSpec weaverSpec, String nodeType) {
         var baseModel = instantiate(baseSpec).buildRaw();
+        var weaverModel = weaverSpec.build();
+
         var baseMemberSignatures = new LinkedHashSet<String>();
         for (var attr : baseModel.getGlobal().getOwnAttributes()) {
             baseMemberSignatures.add(TypeMapper.memberSignature(attr.name(), attr.parameters()));
@@ -43,19 +44,17 @@ public final class WeaverGen2 {
             baseMemberSignatures.add(TypeMapper.memberSignature(action.name(), action.parameters()));
         }
 
-        var model = weaverSpec.build();
-        SpecValidator.validate(model);
+        var mergedModel = SpecMerger.merge(baseModel, weaverModel);
 
         var config = new GeneratorConfig(
-                model.getWeaverName(),
-                model.getBasePackage(),
+                weaverModel.getWeaverName(),
+                weaverModel.getBasePackage(),
                 nodeType,
                 true,
                 true,
-                Set.copyOf(baseMemberSignatures)
-        );
+                Set.copyOf(baseMemberSignatures));
 
-        return new WeaverGen2(model, config);
+        return new WeaverGen2(weaverModel, mergedModel, config);
     }
 
     private static WeaverSpec instantiate(WeaverSpec spec) {
@@ -78,16 +77,17 @@ public final class WeaverGen2 {
                 nodeType,
                 true,
                 false,
-                Set.of()
-        );
+                Set.of());
 
-        return new WeaverGen2(model, config);
+        return new WeaverGen2(model, model, config);
     }
 
     /**
      * Generates all artifacts and writes them to the output directory.
      */
     public void generate(Path outputDir, Path jsonOutPath) throws IOException {
+        var outputModel = config.hasBaseSpec() ? mergedModel : model;
+
         // Abstract join point classes
         for (var jp : model.getAllJpClasses()) {
             var gen = new AbstractJpGenerator(jp, model, config);
@@ -99,24 +99,27 @@ public final class WeaverGen2 {
         // Entities and enums
         var entityGen = new EntityGenerator(model, config);
         for (var entry : entityGen.generateTypeDefs().entrySet()) {
-            writeFile(outputDir, config.entitiesPackage(), TypeMapper.capitalize(entry.getKey()) + ".java", entry.getValue());
+            writeFile(outputDir, config.entitiesPackage(), TypeMapper.capitalize(entry.getKey()) + ".java",
+                    entry.getValue());
         }
         for (var entry : entityGen.generateEnumDefs().entrySet()) {
-            writeFile(outputDir, config.enumsPackage(), TypeMapper.capitalize(entry.getKey()) + ".java", entry.getValue());
+            writeFile(outputDir, config.enumsPackage(), TypeMapper.capitalize(entry.getKey()) + ".java",
+                    entry.getValue());
         }
 
         if (config.hasBaseSpec()) {
             // Abstract weaver
-            var weaverGen = new WeaverAbstractGenerator(model, config);
-            writeFile(outputDir, config.abstractWeaverPackage(), "A" + config.weaverName() + ".java", weaverGen.generate());
+            var weaverGen = new WeaverAbstractGenerator(outputModel, config);
+            writeFile(outputDir, config.abstractWeaverPackage(), "A" + config.weaverName() + ".java",
+                    weaverGen.generate());
 
             // DOT
-            var dot = new DotGenerator(model).generate();
+            var dot = new DotGenerator(outputModel).generate();
             writeFile(outputDir, config.basePackage(), config.weaverName() + ".dotty", dot);
         }
 
         // JSON
-        var json = JsonSerializer.toJson(model);
+        var json = JsonSerializer.toJson(outputModel);
         if (jsonOutPath != null) {
             var parentDir = jsonOutPath.getParent();
             if (parentDir != null) {
@@ -133,7 +136,8 @@ public final class WeaverGen2 {
      */
     public void generateUserAbstract(Path outputDir) throws IOException {
         if (!config.hasBaseSpec()) {
-            return; // Only generate user abstract if we have a base spec (otherwise there's no generated abstract weaver to extend)
+            return; // Only generate user abstract if we have a base spec (otherwise there's no
+                    // generated abstract weaver to extend)
         }
 
         var pkg = config.basePackage() + ".abstracts";
@@ -152,8 +156,9 @@ public final class WeaverGen2 {
         var sb = new StringBuilder();
         sb.append("package ").append(pkg).append(";\n\n");
         sb.append("import ").append(config.joinPointPackage()).append(".")
-            .append(TypeMapper.abstractClassName(model.getGlobal().getName())).append(";\n");
-        sb.append("import ").append(config.abstractWeaverPackage()).append(".").append(config.weaverClassName()).append(";\n\n");
+                .append(TypeMapper.abstractClassName(model.getGlobal().getName())).append(";\n");
+        sb.append("import ").append(config.abstractWeaverPackage()).append(".").append(config.weaverClassName())
+                .append(";\n\n");
         sb.append("/**\n");
         sb.append(" * Abstract class which can be edited by the developer.\n");
         sb.append(" * This class will NOT be overwritten by the generator.\n");
@@ -161,8 +166,9 @@ public final class WeaverGen2 {
         sb.append("public abstract class ").append(config.userAbstractClassName());
         sb.append("<Self extends ").append(config.userAbstractClassName()).append("<Self>>");
         sb.append(" extends ").append(TypeMapper.abstractClassName(model.getGlobal().getName())).append("<Self> {\n\n");
-        sb.append("    public ").append(config.userAbstractClassName()).append("(").append(config.nodeType()).append(" node, ")
-            .append(config.weaverClassName()).append(" weaver) {\n");
+        sb.append("    public ").append(config.userAbstractClassName()).append("(").append(config.nodeType())
+                .append(" node, ")
+                .append(config.weaverClassName()).append(" weaver) {\n");
         sb.append("        super(node, weaver);\n");
         sb.append("    }\n");
         sb.append("}\n");
@@ -179,7 +185,8 @@ public final class WeaverGen2 {
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
-            System.err.println("Usage: WeaverGen2 <specClassName> <outputDir> [--base <baseSpecClassName>] [--node <nodeType>] [--jsonOutPath <jsonFilePath>]");
+            System.err.println(
+                    "Usage: WeaverGen2 <specClassName> <outputDir> [--base <baseSpecClassName>] [--node <nodeType>] [--jsonOutPath <jsonFilePath>]");
             System.exit(1);
         }
 
--- WeaverGen2/src/org/lara/weavergen2/generator/SpecMerger.java
+++ WeaverGen2/src/org/lara/weavergen2/generator/SpecMerger.java
@@ -1,10 +1,14 @@
 package org.lara.weavergen2.generator;
 
 import org.lara.langspec2.model.*;
-import org.lara.langspec2.dsl.WeaverSpec;
 import org.lara.langspec2.types.JpDataType;
+import org.lara.langspec2.types.JpDataType.ArrayType;
+import org.lara.langspec2.types.JpDataType.JpRefType;
+import org.lara.langspec2.types.JpDataType.ParameterizedType;
+import org.lara.langspec2.types.JpDataType.WildcardType;
 
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 /**
@@ -19,137 +23,194 @@ public final class SpecMerger {
 
     /**
      * Merges the base spec into the weaver spec model.
-     * The base spec's global attributes and actions are prepended to the weaver model's global.
+     * The base spec's global attributes and actions are copied into the weaver model's global,
+     * preserving the weaver definition when both specs declare the same member signature.
      */
-    public static WeaverModel merge(WeaverSpec baseSpec, WeaverSpec weaverSpec) {
-        // Build both models raw (no validation yet)
-        var baseModel = baseSpec.buildRaw();
-        var weaverModel = weaverSpec.buildRaw();
+    public static WeaverModel merge(WeaverModel baseModel, WeaverModel weaverModel) {
+        var mergedGlobal = copyJoinPoint(weaverModel.getGlobal());
+        mergeBaseGlobalMembers(baseModel.getGlobal(), mergedGlobal, weaverModel.getGlobal().getName());
 
-        // Merge base global attributes/actions into weaver global
-        var weaverGlobal = weaverModel.getGlobal();
-        var baseGlobal = baseModel.getGlobal();
-
-        var mergedAttributes = mergeAttributes(baseGlobal.getOwnAttributes(), weaverGlobal.getOwnAttributes());
-        var mergedActions = mergeActions(baseGlobal.getOwnActions(), weaverGlobal.getOwnActions());
-
-        // We need to clear and re-add in order to prepend
-        // Since JpClass uses an internal mutable list, we must work around it
-        // by creating a merged model manually.
-        var mergedGlobal = new JpClass("joinpoint");
-        for (var attr : mergedAttributes) {
-            mergedGlobal.addAttribute(attr);
-        }
-        for (var action : mergedActions) {
-            mergedGlobal.addAction(action);
-        }
-        if (weaverGlobal.getDefaultAttribute().isPresent()) {
-            mergedGlobal.setDefaultAttribute(weaverGlobal.getDefaultAttribute().get());
-        }
-
-        // Build merged model
         var merged = new WeaverModel(
                 weaverModel.getWeaverName(),
                 weaverModel.getBasePackage(),
                 mergedGlobal
         );
 
-        // Re-parent all join points: those that pointed to weaverGlobal now point to mergedGlobal
+        var copiedJoinPoints = new LinkedHashMap<String, JpClass>();
+        copiedJoinPoints.put(mergedGlobal.getName(), mergedGlobal);
+
         for (var jp : weaverModel.getJoinPoints().values()) {
-            if (jp.getParent().orElse(null) == weaverGlobal) {
-                jp.setParent(mergedGlobal);
-            }
-            merged.addJoinPoint(jp);
+            copiedJoinPoints.put(jp.getName(), copyJoinPoint(jp));
         }
 
-        // Copy typedefs and enums
-        for (var td : baseModel.getTypeDefs().values()) {
-            merged.addTypeDef(td);
+        for (var jp : weaverModel.getJoinPoints().values()) {
+            var copiedJp = copiedJoinPoints.get(jp.getName());
+            jp.getParent().ifPresent(parent -> copiedJp.setParent(copiedJoinPoints.get(parent.getName())));
+            merged.addJoinPoint(copiedJp);
         }
+
         for (var td : weaverModel.getTypeDefs().values()) {
+            merged.addTypeDef(copyTypeDef(td));
+        }
+        for (var td : baseModel.getTypeDefs().values()) {
             if (!merged.getTypeDefs().containsKey(td.name())) {
-                merged.addTypeDef(td);
+                merged.addTypeDef(copyTypeDef(td));
             }
         }
 
-        for (var ed : baseModel.getEnumDefs().values()) {
-            merged.addEnumDef(ed);
-        }
         for (var ed : weaverModel.getEnumDefs().values()) {
+            merged.addEnumDef(copyEnumDef(ed));
+        }
+        for (var ed : baseModel.getEnumDefs().values()) {
             if (!merged.getEnumDefs().containsKey(ed.name())) {
-                merged.addEnumDef(ed);
+                merged.addEnumDef(copyEnumDef(ed));
             }
         }
 
-        // Set root
-        weaverModel.getRoot().ifPresent(merged::setRoot);
+        weaverModel.getRoot().ifPresent(root -> {
+            var copiedRoot = copiedJoinPoints.get(root.getName());
+            if (copiedRoot != null) {
+                merged.setRoot(copiedRoot);
+            }
+        });
+
+        if (merged.getRoot().isEmpty()) {
+            baseModel.getRoot().ifPresent(root -> {
+                var copiedRoot = copiedJoinPoints.get(root.getName());
+                if (copiedRoot != null) {
+                    merged.setRoot(copiedRoot);
+                } else if (root.equals(baseModel.getGlobal())) {
+                    merged.setRoot(mergedGlobal);
+                }
+            });
+        }
 
         return merged;
     }
 
-    private static List<Attribute> mergeAttributes(List<Attribute> baseAttrs, List<Attribute> weaverAttrs) {
-        var merged = new ArrayList<>(baseAttrs);
+    private static void mergeBaseGlobalMembers(JpClass baseGlobal, JpClass mergedGlobal, String targetGlobalName) {
+        var attributeSignatures = new LinkedHashSet<MemberSignature>();
+        for (var attr : mergedGlobal.getOwnAttributes()) {
+            attributeSignatures.add(signature(attr.name(), attr.parameters()));
+        }
 
-        for (var weaverAttr : weaverAttrs) {
-            var baseIndex = findMatchingAttributeIndex(merged, weaverAttr);
-            if (baseIndex < 0) {
-                merged.add(weaverAttr);
+        for (var baseAttr : baseGlobal.getOwnAttributes()) {
+            var rewrittenAttr = copyAttribute(baseAttr, baseGlobal.getName(), targetGlobalName);
+            var signature = signature(rewrittenAttr.name(), rewrittenAttr.parameters());
+            if (attributeSignatures.contains(signature)) {
                 continue;
             }
 
-            var baseAttr = merged.get(baseIndex);
-            var effectiveTooltip = weaverAttr.tooltip() != null ? weaverAttr.tooltip() : baseAttr.tooltip();
-            merged.set(baseIndex, new Attribute(weaverAttr.name(), weaverAttr.type(), weaverAttr.parameters(), effectiveTooltip));
+            mergedGlobal.addAttribute(rewrittenAttr);
+            attributeSignatures.add(signature);
         }
 
-        return List.copyOf(merged);
-    }
+        var actionSignatures = new LinkedHashSet<MemberSignature>();
+        for (var action : mergedGlobal.getOwnActions()) {
+            actionSignatures.add(signature(action.name(), action.parameters()));
+        }
 
-    private static List<Action> mergeActions(List<Action> baseActions, List<Action> weaverActions) {
-        var merged = new ArrayList<>(baseActions);
-
-        for (var weaverAction : weaverActions) {
-            var baseIndex = findMatchingActionIndex(merged, weaverAction);
-            if (baseIndex < 0) {
-                merged.add(weaverAction);
+        for (var baseAction : baseGlobal.getOwnActions()) {
+            var rewrittenAction = copyAction(baseAction, baseGlobal.getName(), targetGlobalName);
+            var signature = signature(rewrittenAction.name(), rewrittenAction.parameters());
+            if (actionSignatures.contains(signature)) {
                 continue;
             }
 
-            var baseAction = merged.get(baseIndex);
-            var effectiveTooltip = weaverAction.tooltip() != null ? weaverAction.tooltip() : baseAction.tooltip();
-            merged.set(baseIndex, new Action(
-                    weaverAction.name(),
-                    weaverAction.returnType(),
-                    weaverAction.parameters(),
-                    effectiveTooltip
-            ));
+            mergedGlobal.addAction(rewrittenAction);
+            actionSignatures.add(signature);
         }
 
-        return List.copyOf(merged);
+
+        baseGlobal.getDefaultAttribute().ifPresent(defAttr -> {
+            if (mergedGlobal.getDefaultAttribute().isEmpty()) {
+                mergedGlobal.setDefaultAttribute(defAttr);
+            }
+        });
+
+        baseGlobal.getTooltip().ifPresent(tt -> {
+            if (mergedGlobal.getTooltip().isEmpty()) {
+                mergedGlobal.setTooltip(tt);
+            }
+        });
     }
 
-    private static int findMatchingAttributeIndex(List<Attribute> attributes, Attribute candidate) {
-        var candidateSignature = signature(candidate.name(), candidate.parameters());
-        for (int i = 0; i < attributes.size(); i++) {
-            var current = attributes.get(i);
-            if (signature(current.name(), current.parameters()).equals(candidateSignature)) {
-                return i;
-            }
+    private static JpClass copyJoinPoint(JpClass source) {
+        var copy = new JpClass(source.getName());
+        source.getTooltip().ifPresent(copy::setTooltip);
+        source.getDefaultAttribute().ifPresent(copy::setDefaultAttribute);
+
+        for (var attr : source.getOwnAttributes()) {
+            copy.addAttribute(attr);
+        }
+        for (var action : source.getOwnActions()) {
+            copy.addAction(action);
         }
 
-        return -1;
+        return copy;
     }
 
-    private static int findMatchingActionIndex(List<Action> actions, Action candidate) {
-        var candidateSignature = signature(candidate.name(), candidate.parameters());
-        for (int i = 0; i < actions.size(); i++) {
-            var current = actions.get(i);
-            if (signature(current.name(), current.parameters()).equals(candidateSignature)) {
-                return i;
+    private static Attribute copyAttribute(Attribute attr, String fromGlobalName, String toGlobalName) {
+        return new Attribute(
+                attr.name(),
+                rewriteBaseJoinPointRefs(attr.type(), fromGlobalName, toGlobalName),
+                rewriteParameters(attr.parameters(), fromGlobalName, toGlobalName),
+                attr.tooltip()
+        );
+    }
+
+    private static Action copyAction(Action action, String fromGlobalName, String toGlobalName) {
+        return new Action(
+                action.name(),
+                rewriteBaseJoinPointRefs(action.returnType(), fromGlobalName, toGlobalName),
+                rewriteParameters(action.parameters(), fromGlobalName, toGlobalName),
+                action.tooltip()
+        );
+    }
+
+    private static TypeDef copyTypeDef(TypeDef typeDef) {
+        return new TypeDef(typeDef.name(), typeDef.fields(), typeDef.tooltip());
+    }
+
+    private static EnumDef copyEnumDef(EnumDef enumDef) {
+        return new EnumDef(enumDef.name(), enumDef.values(), enumDef.tooltip());
+    }
+
+    private static List<Parameter> rewriteParameters(List<Parameter> parameters, String fromGlobalName, String toGlobalName) {
+        return parameters.stream()
+                .map(parameter -> new Parameter(
+                        parameter.name(),
+                        rewriteBaseJoinPointRefs(parameter.type(), fromGlobalName, toGlobalName),
+                        parameter.defaultValue()))
+                .toList();
+    }
+
+    private static JpDataType rewriteBaseJoinPointRefs(JpDataType type, String fromGlobalName, String toGlobalName) {
+        if (type instanceof JpRefType ref) {
+            if (ref.jpName().equals(fromGlobalName)) {
+                return new JpRefType(toGlobalName);
             }
+            return type;
         }
 
-        return -1;
+        if (type instanceof ArrayType arr) {
+            return new ArrayType(rewriteBaseJoinPointRefs(arr.element(), fromGlobalName, toGlobalName));
+        }
+
+        if (type instanceof ParameterizedType pt) {
+            var base = rewriteBaseJoinPointRefs(pt.base(), fromGlobalName, toGlobalName);
+            var args = pt.args().stream()
+                    .map(arg -> rewriteBaseJoinPointRefs(arg, fromGlobalName, toGlobalName))
+                    .toList();
+            return new ParameterizedType(base, args);
+        }
+
+        if (type instanceof WildcardType wt) {
+            var bound = wt.bound() == null ? null : rewriteBaseJoinPointRefs(wt.bound(), fromGlobalName, toGlobalName);
+            return new WildcardType(wt.kind(), bound);
+        }
+
+        return type;
     }
 
     private static MemberSignature signature(String name, List<Parameter> parameters) {
