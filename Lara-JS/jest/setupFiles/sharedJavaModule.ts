c1ffb2e2c6cff22d980d1a173d8260d6bf765e4e
L. Sousa <lm.sousa@fe.up.pt>
2026-06-09 16:45:04 +0100

    Lara-JS: Add proper exports to the package and cleanup tsconfig

--- Lara-JS/jest/setupFiles/sharedJavaModule.ts
+++ Lara-JS/jest/setupFiles/sharedJavaModule.ts
@@ -1,5 +1,7 @@
-/* global global */
-
 import { jest } from "@jest/globals";
+
+declare global {
+  var __SHARED_MODULE__: typeof import("java");
+}
 
 jest.mock("java", () => global.__SHARED_MODULE__);
